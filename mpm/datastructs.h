1dd9eba3685b64146bced370e93e7793bca836ae
gstrube <gstrube@870f116b-6601-0410-b2b2-9324e3df3746>
2002-12-29 19:50:50 +0000

    Swapped Unary < and > logic. < is mod256 (LSB), > is div256 (MSB).

--- mpm/datastructs.h
+++ mpm/datastructs.h
@@ -44,7 +44,7 @@ enum flag           { OFF, ON };
 
 enum symbols        { space, bin_and, dquote, squote, semicolon, comma, fullstop, lparen, lcurly, rcurly, rparen,
                       plus, minus, multiply, divi, mod, bin_xor, assign, bin_or, bin_nor, colon = bin_nor, bin_not, less,
-                      div256 = less, greater, mod256 = greater, log_not, hash, constexpr, newline, power, lshift, rshift,
+                      mod256 = less, greater, div256 = greater, log_not, hash, constexpr, newline, power, lshift, rshift,
                       lessequal, greatequal, notequal, name, number, decmconst, hexconst, binconst, charconst, registerid,
                       strconq = fullstop, negated, nil, ifstatm, elsestatm, endifstatm
                     };
