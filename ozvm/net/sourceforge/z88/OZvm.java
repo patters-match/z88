23dace919a36782cee744eeb0389d5ed32b34151
gstrube <gstrube@870f116b-6601-0410-b2b2-9324e3df3746>
2003-01-04 01:00:04 +0000

    Hurraayyy! Main entry now successfully initializes Z88 system and loads the external ROM!

--- ozvm/net/sourceforge/z88/OZvm.java
+++ ozvm/net/sourceforge/z88/OZvm.java
@@ -1,5 +1,8 @@
 package net.sourceforge.z88;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * @author Gunther Strube
  *
@@ -13,6 +16,28 @@ public class OZvm {
 	static private final String defaultRomImage = "z88.rom";
 	
 	public static void main(String[] args) {
+		Z88 z88 = null;
+		
+		try {
+			z88 = new Z88();	// Instantiate a Z88 environment with default 128K RAM
+		} catch (Exception e) {
+			System.out.println("Couldn't initialize Z88 virtual machine.");
+			return;
+		}
+		
+		try {
+			if (args.length == 0)
+				z88.loadRom(defaultRomImage);
+			else
+				z88.loadRom(args[0]);
+		} catch (FileNotFoundException e) {
+			System.out.println("Couldn't load ROM image.");
+			return;
+		} catch (IOException e) {
+			System.out.println("Problem with ROM image or I/O");
+			return;
+		}
+		
 		System.out.println("OZvm V0.01, Z88 Virtual Machine");
 	}
 }
