6c3325393c83ca00775659bde929d752075b39c0
zoksanen <zoksanen@870f116b-6601-0410-b2b2-9324e3df3746>
2003-01-05 22:33:23 +0000

    original 4.01 code

---
+++ oz/bank0/pfilter0.asm
@@ -0,0 +1,59 @@
+; -----------------------------------------------------------------------------
+; Bank 0 @ S3           ROM offset $39a5
+;
+; $Id$
+; -----------------------------------------------------------------------------
+
+        module  PFilter
+
+        include "all.def"
+        include "sysvar.def"
+
+        org     $f9a5
+
+defc    MS2BankA        =$d721
+defc    PrntChar        =$afc0
+
+xdef    PrFilterCall
+xdef    TogglePrFilter
+
+; Call printer filter in bank 3
+; 
+; A=C0/C3/C6
+
+.PrFilterCall
+
+        ld      h, a                            ; remember A
+        ld      a, (BLSC_SR2)                   ; remember S2
+        push    af
+        ld      a, OZBANK_PRFILTER              ; bind code in S2
+        call    MS2BankA
+        ld      a, h
+
+        ld      h, >PrntChar
+        push    ix
+        call    $00df                           ; jp (hl)
+        pop     ix
+
+        ex      af, af'
+        pop     af                              ; restore S1
+        call    MS2BankA
+        ex      af, af'
+        ret
+
+;       ----
+
+; printer filter enable/disable
+
+.TogglePrFilter 
+        push    hl
+        ld      a, (ubScreenBase)               ; screen base bank
+        ld      b, a
+        ld      hl, sbf_VDU1&$3fff
+        ld      a, (sbf_PrefixSeq)
+        dec     a                               ; -1 for '.'
+        ld      c, a                            ; length
+        OZ      DC_Gen
+        pop     hl
+        ret
+
