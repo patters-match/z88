2bd72188b6f4d33da864a4f6a56640a3c173c098
zoksanen <zoksanen@870f116b-6601-0410-b2b2-9324e3df3746>
2003-06-21 16:57:06 +0000

    path changes in assembly instructions

--- oz/kernel.asm
+++ oz/kernel.asm
@@ -10,13 +10,13 @@
 ; this file is here just to give binary correct name
 
 ; to assemble:
-;       mpm -bvg -IC:\z88\ozdef bank7\lowram.asm
-;       mpm -bvg -IC:\z88\ozdef bank7\appdors.asm
-;       mpm -bvg -IC:\z88\ozdef @kernel.prj
+;       mpm -bvg -IC:\CVS\oz\sysdef bank7\lowram.asm
+;       mpm -bvg -IC:\CVS\oz\sysdef bank7\appdors.asm
+;       mpm -bvg -IC:\CVS\oz\sysdef @kernel.prj
 ;
 ; now we tell lowram.asm real kernel addresses
-;       mpm -bv -DFINAL -IC:\z88\ozdef bank7\lowram.asm
+;       mpm -bv -DFINAL -IC:\CVS\oz\sysdef bank7\lowram.asm
 ;
 ; and insert lowram.bin into kernel
-;       mpm -bv -IC:\z88\ozdef @kernel.prj
+;       mpm -bv -IC:\CVS\oz\sysdef @kernel.prj
 
