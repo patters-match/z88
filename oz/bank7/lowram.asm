f21ec1f536a5e57c6d21edb7277f01794e86fce3
zoksanen <zoksanen@870f116b-6601-0410-b2b2-9324e3df3746>
2003-06-25 10:45:08 +0000

    documented OZ_DI even more

--- oz/bank7/lowram.asm
+++ oz/bank7/lowram.asm
@@ -35,7 +35,7 @@ xdef    OZ_RET0
 xdef    OZ_BUF
 xdef    OZ_DI
 xdef    OZ_EI
-xdef    OZ_SCF
+xdef    OZ_INT
 xdef    OZCallJump
 xdef    OZCallReturn1
 xdef    OZCallReturn2
@@ -67,7 +67,7 @@ xdef    OZCallReturn3
 
         defb    $ff,$ff
 .FPP_RET
-        jp      OZCallReturn4                   ; 001d, called from FPP
+        jp      OZCallReturnFP                  ; 001d, called from FPP
 
 .rst20
         jp      CallOZMain                      ; 0020
@@ -87,7 +87,9 @@ xdef    OZCallReturn3
 
         defb    $ff,$ff,$ff,$ff,$ff
 
-.rst38
+;       hardware INT entry point
+
+.OZINT
         push    af
         ld      a, (BLSC_SR3)                   ; remember S3 and bind in b00
         push    af
@@ -95,7 +97,8 @@ xdef    OZCallReturn3
         ld      (BLSC_SR3), a
         out     (BL_SR3), a
         in      a, (BL_STA)                     ; get interrupt status and execute inthandler
-        jp      INTEntry
+        jp      INTEntry                        ; !! 'in a' needs to be moved into ROM code
+                                                ; !! if code we use other banks
 
 ;       OZ low level jump table
 
@@ -109,10 +112,12 @@ xdef    OZCallReturn3
         jp      OZDImain                        ; 0051
 .OZ_EI
         jp      OZEImain                        ; 0054
-.OZ_SCF
+.OZ_INT
         jp      OZSCFmain                       ; 0057
 
         defs    4*3 ($ff)
+
+;       hardware NMI entry point
 
 .OZNMI
         push    af                              ; 0066
@@ -127,22 +132,23 @@ xdef    OZCallReturn3
         pop     hl
         jr      z, rst00
 
-        ld      a, i                            ; store int disable count
+        ld      a, i                            ; store int status
         push    af
 
-        di
+        di                                      ; nested NMIs won't enable interrupts
         ld      a, (BLSC_SR3)                   ; remember S3 and bind in b00
         push    af
         xor     a
         ld      (BLSC_SR3), a
         out     (BL_SR3), a
-        call    NMIEntry                        ; call nmihandler
+
+        call    NMIEntry                        ; call NMI handler
 
         pop     af                              ; restore S3
         ld      (BLSC_SR3), a
         out     (BL_SR3), a
 
-        pop     af
+        pop     af                              ; !! can't use 'retn' because of 'di' above
         jp      po, noEI                        ; ints were disabled
         pop     af
         ei
@@ -153,6 +159,9 @@ xdef    OZCallReturn3
         ret
 
 ; 0095
+;
+;       called from misc2.asm
+
 .OZCallJump
         pop     af                              ; restore S3
         ld      (BLSC_SR3), a
@@ -161,6 +170,9 @@ xdef    OZCallReturn3
         ret
 
 ; 009d
+;
+;       called from int.asm
+
 .INTReturn
         pop     af                              ; restore S3
         ld      (BLSC_SR3), a
@@ -180,21 +192,28 @@ xdef    OZCallReturn3
 ; 00ab          ret with AFBCDEHL
 
 .OZCallReturn1
+;
+;       called from buffer.asm, memory.asm, misc4.asm, ossi.asm
+
         exx
 
 ; 00ac          ret with AFbcdehl
+;
+;       called from buffer.asm, error.asm, esc.asm, memory.asm, misc2.asm, oscli0.asm
 
 .OZCallReturn2
         ex      af, af'
 
 ; 00ad          ret with afbcdehl
+;
+;       called from buffer.asm
 
 .OZCallReturn3
         exx
 
 ; 00ae          ret with afBCDEHL
 
-.OZCallReturn4
+.OZCallReturnFP
         pop     af
         scf
 
@@ -234,6 +253,9 @@ xdef    OZCallReturn3
         ret
 
 ; 00d4
+;
+;       called from error.asm, process3.asm
+
 .JpAHL
         call    MS3BankA
         ex      af, af'
@@ -246,11 +268,16 @@ xdef    OZCallReturn3
         jr      MS3BankA
 
 ; 00df
+;
+;       called from pfilter0.asm (could use elsewhere as well)
 
 .JpHL
         jp      (hl)                            ; !! can use one in CallOZMain
 
 ; 00e0
+;
+;       referenced from error.asm, process3.asm
+
 .DefErrHandler
         ret     z
         cp      a
@@ -374,6 +401,8 @@ xdef    OZCallReturn3
 
 ; 0177
 
+;       called from int.asm thru OZ_INT to handle spurious interrupts
+
 .OZSCFmain
         scf
         ret
@@ -393,6 +422,8 @@ xdef    OZCallReturn3
         ret
 
 ; 0185
+;
+;       called thru $0025, maybe unused
 
 .CallOZret
         pop     bc                              ; restore bank B into segment C
@@ -412,22 +443,22 @@ xdef    OZCallReturn3
 .OZDImain
         xor     a                               ; A=0, Fc=0
         push    af                              ; store A to clear byte in stack
-        pop     af                              ; 
+        pop     af                              ;
         ld      a, i                            ; A=snooze/coma flag, IFF2 -> Fp
         di
         ret     pe                              ; interrupts enabled? exit Fc=0
 
-;       we have two possible cases here:
+;       we have three possible cases here:
 ;
-;       a) interrupts were disabled all the time
-;       b) we got interrupt during 'ld a, i' so interrupts were on
+;       a)  we got interrupt during 'ld a, i' so interrupts were on
+;       b1) interrupts were disabled all the time
+;       b2) interrupts were disabled, but somewhere here NMI happens
 ;
-;       in case (a) we read back zero, in case (b) it was overwritten by PC
-;       high byte , which is 1 in our case
-
+;       in cases (a) and (b2) zero in stack was overwritten with PC high byte,
+;       in case (b1) we read back zero.  !! (b2) isn't handled
 
         dec     sp                              ; read back A
-        dec     sp                              ;
+        dec     sp
         pop     af
 
 ;       !! if we assert 'DI' above is at odd address, Fc is correct without compare
