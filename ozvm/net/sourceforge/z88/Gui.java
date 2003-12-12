08769055476e31540764eb5b61f4e7aad3aee28b
gstrube <gstrube@870f116b-6601-0410-b2b2-9324e3df3746>
2003-12-12 14:32:17 +0000

    First take on Java Swing GUI. Still a simple window with a canvas for the Z88 screen.

---
+++ ozvm/net/sourceforge/z88/Gui.java
@@ -0,0 +1,90 @@
+/*
+ * Created on Dec 12, 2003
+ *
+ * To change the template for this generated file go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+package net.sourceforge.z88;
+
+import javax.swing.JFrame;
+
+/**
+ * @author gbs
+ *
+ * To change the template for this generated type comment go to
+ * Window&gt;Preferences&gt;Java&gt;Code Generation&gt;Code and Comments
+ */
+public class Gui extends JFrame {
+
+	private javax.swing.JPanel jContentPane = null;
+
+	private java.awt.Canvas canvas = null;
+	/**
+	 * This is the default constructor
+	 */
+	public Gui() {
+		super();
+		initialize();
+	}
+	/**
+	 * This method initializes this
+	 * 
+	 * @return void
+	 */
+	private void initialize() {
+		this.setSize(648, 96);
+		this.setContentPane(getJContentPane());
+		this.setTitle("OZvm");
+		this.addWindowListener(new java.awt.event.WindowAdapter() { 
+			public void windowClosing(java.awt.event.WindowEvent e) {    
+				System.out.println("windowClosing()"); // TODO Auto-generated Event stub windowClosing()
+				System.exit(0);				
+			}
+		});
+	}
+	/**
+	 * This method initializes jContentPane
+	 * 
+	 * @return javax.swing.JPanel
+	 */
+	private javax.swing.JPanel getJContentPane() {
+		if (jContentPane == null) {
+			jContentPane = new javax.swing.JPanel();
+			jContentPane.setLayout(new java.awt.BorderLayout());
+			jContentPane.add(getCanvas(), java.awt.BorderLayout.CENTER);
+		}
+		return jContentPane;
+	}
+	/**
+	 * This method initializes canvas
+	 * 
+	 * @return java.awt.Canvas
+	 */
+	private java.awt.Canvas getCanvas() {
+		if(canvas == null) {
+			canvas = new java.awt.Canvas();
+			canvas.setSize(640,64);
+			canvas.setVisible(true);
+		}
+		return canvas;
+	}
+	/**
+	 * @param canvas
+	 */
+	public void setCanvas(java.awt.Canvas canvas) {
+		this.canvas = canvas;
+	}
+
+	public static void main(String[] args) {
+		Gui gg = new Gui();
+		gg.show();
+
+		OZvm ozvm = new OZvm(gg.getCanvas());
+		if (ozvm.loadRoms(args) == false) {
+			System.out.println("Ozvm terminated.");
+			System.exit(0);
+		}
+		
+		ozvm.run();
+	}
+}  //  @jve:visual-info  decl-index=0 visual-constraint="10,10"
