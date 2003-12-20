c9bf9791702c2f15510174e04f32de2348c04339
gstrube <gstrube@870f116b-6601-0410-b2b2-9324e3df3746>
2003-12-20 23:03:09 +0000

    Toggling a breakpoint shouldn't automatically display the list of currently defined breakpoints. This functionality is handled from the caller.

--- ozvm/net/sourceforge/z88/Breakpoints.java
+++ ozvm/net/sourceforge/z88/Breakpoints.java
@@ -47,8 +47,6 @@ public class Breakpoints {
             breakPoints.put((Breakpoint) bp, (Breakpoint) bp);
         else
             breakPoints.remove((Breakpoint) bp);
-
-        listBreakpoints();
     }
 
     /**
