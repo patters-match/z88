d0e5b2f6b92d0b2974547801294f597b2efa6d2c
gstrube <gstrube@870f116b-6601-0410-b2b2-9324e3df3746>
2004-01-02 12:01:14 +0000

    Bug fix: listing breakpoints displayed a "[d]" for stop breakpoints. Breakpoint addresses now displayed as 24 bit addresses.

--- ozvm/net/sourceforge/z88/Breakpoints.java
+++ ozvm/net/sourceforge/z88/Breakpoints.java
@@ -12,17 +12,17 @@ import java.util.Iterator;
  * $Id$
  *
  * Created on July 11, 2003, 5:42 PM
- * 
+ *
  */
 public class Breakpoints {
     private Map breakPoints = null;
-    Blink blink; 
+    Blink blink;
 
     /**
      * Just instantiate this Breakpoint Manager
      */
     Breakpoints(Blink b) {
-        breakPoints = new HashMap(); 
+        breakPoints = new HashMap();
         blink = b;
     }
 
@@ -37,40 +37,53 @@ public class Breakpoints {
 
 	/**
 	 * Add (if not created) or remove breakpoint (if prev. created).
-	 * 
-	 * @param address 24bit extended address 
+	 *
+	 * @param address 24bit extended address
 	 */
 	public void toggleBreakpoint(int bpAddress) {
 		int bpBank = bpAddress >>> 16;
-		bpAddress &= 0xFFFF; 
-		
+		bpAddress &= 0xFFFF;
+
 		toggleBreakpoint(bpAddress, bpBank);
 	}
 
 	/**
+	 * Add (if not created) or remove breakpoint (if prev. created).
+	 *
+	 * @param address 24bit extended address
+	 * @param stopStatus
+	 */
+	public void toggleBreakpoint(int bpAddress, boolean stopStatus ) {
+		int bpBank = bpAddress >>> 16;
+		bpAddress &= 0xFFFF;
+
+		toggleBreakpoint(bpAddress, bpBank, stopStatus);
+	}
+
+	/**
 	 * Get the original Z80 opcode, located at this breakpoint
-	 * 
-	 * @param address 24bit extended (breakpoint) address 
+	 *
+	 * @param address 24bit extended (breakpoint) address
 	 * @return Z80 opcode of breakpoint, or -1 if breakpoint wasn't found
 	 */
 	public int getOrigZ80Opcode(int bpAddress) {
 		Breakpoint bp = new Breakpoint(bpAddress);
 		Breakpoint bpv = (Breakpoint) breakPoints.get(bp);
 		if (bpv != null) {
-			return bpv.getCopyOfOpcode(); 
+			return bpv.getCopyOfOpcode();
 		} else
 			return -1;
 	}
 
     /**
      * Add (if not created) or remove breakpoint (if prev. created).
-     * 
+     *
      * @param offset
      * @param bank
      */
     public void toggleBreakpoint(int offset, int bank) {
         Breakpoint bp = new Breakpoint(offset, bank);
-        if (breakPoints.containsKey( (Breakpoint) bp) == false) 
+        if (breakPoints.containsKey( (Breakpoint) bp) == false)
             breakPoints.put((Breakpoint) bp, (Breakpoint) bp);
         else
             breakPoints.remove((Breakpoint) bp);
@@ -78,35 +91,35 @@ public class Breakpoints {
 
 	/**
 	 * Add (if not created) or remove breakpoint (if prev. created).
-	 * 
+	 *
 	 * @param offset
 	 * @param bank
 	 * @param stopStatus
 	 */
 	public void toggleBreakpoint(int offset, int bank, boolean stopStatus) {
 		Breakpoint bp = new Breakpoint(offset, bank, stopStatus);
-		if (breakPoints.containsKey( (Breakpoint) bp) == false) 
+		if (breakPoints.containsKey( (Breakpoint) bp) == false)
 			breakPoints.put((Breakpoint) bp, (Breakpoint) bp);
 		else
 			breakPoints.remove((Breakpoint) bp);
 	}
 
 	/**
-	 * Return <true> if breakpoint will stop exection. 
-	 * 
+	 * Return <true> if breakpoint will stop exection.
+	 *
 	 * @param bpAddress 24bit extended address
 	 * @return true, if breakpoint is defined to stop execution.
 	 */
 	public boolean isStoppable(int bpAddress) {
 		int bpBank = bpAddress >>> 16;
-		bpAddress &= 0xFFFF; 
+		bpAddress &= 0xFFFF;
 
-		return isStoppable(bpAddress, bpBank); 
+		return isStoppable(bpAddress, bpBank);
 	}
 
 	/**
-	 * Return <true> if breakpoint will stop exection. 
-	 * 
+	 * Return <true> if breakpoint will stop exection.
+	 *
 	 * @param offset
 	 * @param bank
 	 * @return true, if breakpoint is defined to stop execution.
@@ -115,7 +128,7 @@ public class Breakpoints {
 		Breakpoint bp = new Breakpoint(offset, bank);
 		Breakpoint bpv = (Breakpoint) breakPoints.get(bp);
 		if (bpv != null && bpv.stop == true) {
-			return true; 
+			return true;
 		} else
 			return false;
 	}
@@ -130,39 +143,37 @@ public class Breakpoints {
             Iterator keyIterator = breakPoints.entrySet().iterator();
 
             while(keyIterator.hasNext()) {
-                Map.Entry e = (Map.Entry) keyIterator.next();   
+                Map.Entry e = (Map.Entry) keyIterator.next();
                 Breakpoint bp = (Breakpoint) e.getKey();
 
-                int offset = bp.getBpAddress() & 0xFFFF;
-                int bank = bp.getBpAddress() >>> 16;
-                System.out.print(Dz.addrToHex(offset,false) + "," + Dz.byteToHex(bank,false) + (bp.stop == true ? "[d]" : "") + "\t"); 
+                System.out.print(Dz.extAddrToHex(bp.getBpAddress(),false) + (bp.stop == false ? "[d]" : "") + "\t");
             }
             System.out.println();
         }
     }
 
     /**
-     * Set the "breakpoint" instruction in Z88 memory for all 
+     * Set the "breakpoint" instruction in Z88 memory for all
      * currently defined breakpoints.
-     */		
+     */
     public void setBreakpoints() {
-        // now, set the breakpoint at the extended address; 
+        // now, set the breakpoint at the extended address;
         // the instruction opcode 40 ("LD B,B"; quite useless!).
-        // which this virtual machine identifies as a "suspend" Z80 exection. 
+        // which this virtual machine identifies as a "suspend" Z80 exection.
         if (breakPoints.isEmpty() == false) {
             Iterator keyIterator = breakPoints.entrySet().iterator();
 
             while(keyIterator.hasNext()) {
-                Map.Entry e = (Map.Entry) keyIterator.next();   
+                Map.Entry e = (Map.Entry) keyIterator.next();
                 Breakpoint bp = (Breakpoint) e.getKey();
 
                 int offset = bp.getBpAddress() & 0x3FFF;
                 int bank = bp.getBpAddress() >>> 16;
-                
+
                 if (bp.stop == true) {
 					blink.setByte(offset, bank, 0x40);	// use "LD B,B" as stop breakpoint
                 } else {
-					blink.setByte(offset, bank, 0x49);	// use "LD C,C" as display breakpoint 
+					blink.setByte(offset, bank, 0x49);	// use "LD C,C" as display breakpoint
                 }
             }
         }
@@ -173,41 +184,41 @@ public class Breakpoints {
      * that was overwritten by the "breakpoint" instruction in Z88 memory.
      */
     public void clearBreakpoints() {
-        // restore at the extended address; 
-        // the original instruction opcode that is preserved inside 
-        // the BreakPoint object 
+        // restore at the extended address;
+        // the original instruction opcode that is preserved inside
+        // the BreakPoint object
         if (breakPoints.isEmpty() == false) {
             Iterator keyIterator = breakPoints.entrySet().iterator();
 
             while(keyIterator.hasNext()) {
-                Map.Entry e = (Map.Entry) keyIterator.next();   
+                Map.Entry e = (Map.Entry) keyIterator.next();
                 Breakpoint bp = (Breakpoint) e.getKey();
 
                 int offset = bp.getBpAddress() & 0x3FFF;
                 int bank = bp.getBpAddress() >>> 16;
 
-                // restore the original opcode bit pattern... 
+                // restore the original opcode bit pattern...
                 blink.setByte(offset, bank, bp.getCopyOfOpcode() & 0xFF);
             }
-        }			
+        }
     }
 
     // The breakpoint container.
     private class Breakpoint {
         int addressKey;			// the 24bit address of the breakpoint
         int instr;				// the original 8bit opcode at breakpoint
-        boolean stop;			
+        boolean stop;
 
 		/**
 		 * Create a breakpoint object.
-		 * 
+		 *
 		 * @param offset 16bit offset within bank
-		 * @param bank 16K memory block number  
+		 * @param bank 16K memory block number
 		 */
         Breakpoint(int offset, int bank) {
 			// default is to stop execution at breakpoint
 			stop = true;
-			
+
             // the encoded key for the SortedSet...
             addressKey = (bank << 16) | offset;
 
@@ -217,13 +228,13 @@ public class Breakpoints {
 
 		/**
 		 * Create a breakpoint object.
-		 * 
+		 *
 		 * @param bpAddress 24bit extended address
 		 */
 		Breakpoint(int bpAddress) {
 			// default is to stop execution at breakpoint
 			stop = true;
-			
+
 			// the encoded key for the SortedSet...
 			addressKey = bpAddress;
 
@@ -234,7 +245,7 @@ public class Breakpoints {
 		Breakpoint(int offset, int bank, boolean stopAtAddress) {
 			// use <false> to display register status, then continue, <true> to stop execution.
 			stop = stopAtAddress;
-			
+
 			// the encoded key for the SortedSet...
 			addressKey = (bank << 16) | offset;
 
@@ -251,29 +262,29 @@ public class Breakpoints {
         }
 
         private void setCopyOfOpcode(int z80instr) {
-            instr = z80instr;				
+            instr = z80instr;
         }
 
         private int getCopyOfOpcode() {
-            return instr; 
+            return instr;
         }
 
-        // override interface with the actual implementation for this object. 
+        // override interface with the actual implementation for this object.
         public int hashCode() {
             return addressKey;	// the unique key is a perfect hash code
         }
 
-        // override interface with the actual implementation for this object. 
+        // override interface with the actual implementation for this object.
         public boolean equals(Object bp) {
             if (!(bp instanceof Breakpoint)) {
                 return false;
             }
 
-            Breakpoint aBreakpoint = (Breakpoint) bp; 
+            Breakpoint aBreakpoint = (Breakpoint) bp;
             if (addressKey == aBreakpoint.addressKey)
                 return true;
-            else 
-                return false; 
+            else
+                return false;
         }
     }
 }
