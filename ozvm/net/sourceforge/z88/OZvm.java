0c5c3b5b539232e7234ce02d556ffa37312ef7fc
gstrube <gstrube@870f116b-6601-0410-b2b2-9324e3df3746>
2004-01-02 12:46:12 +0000

    OZvm V0.2.2

--- ozvm/net/sourceforge/z88/OZvm.java
+++ ozvm/net/sourceforge/z88/OZvm.java
@@ -17,7 +17,7 @@ import javax.swing.JTextField;
  */
 public class OZvm {
 
-	public static final String VERSION = "0.2.1";
+	public static final String VERSION = "0.2.2";
 	private static final String CMDLINEPROMPT = "OZvm$";
 
 	private Blink z88 = null;
