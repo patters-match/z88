55c45b609524a8f1b7bc753cd1ceefe34ddb7b6b
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2004-09-05 10:04:14 +0000

    API change; BHL pointer to File Entry points into card at slot X. Segment independence. Removed check for File Eprom, we're at File Entry level. Safety is the responsibility of the application.

--- stdlib/eprfetch.asm
+++ stdlib/eprfetch.asm
@@ -17,10 +17,8 @@
 ;
 ;***************************************************************************************************
 
-     LIB FileEprRequest
-     LIB FileEprFileImage
-     LIB FileEprFileSize
-     LIB MemDefBank
+     LIB FileEprRequest, FileEprFileImage, FileEprFileSize
+     LIB SafeBHLSegment, MemDefBank
 
      INCLUDE "fileio.def"
      INCLUDE "integer.def"
@@ -32,20 +30,24 @@
 ; Standard Z88 File Eprom Format, including support for sub File Eprom
 ; Area in application cards (below application banks in first free 64K boundary)
 ;
-; Fetch file (image) from File Eprom, identified by File Entry at BHL, slot C
-; (B=00h-3Fh, HL=0000h-3FFFh)
+; Fetch file (image) from File Eprom Card, identified by File Entry at BHL
+; (B=00h-FFh embedded slot mask, HL=0000h-3FFFh bank offset) and store it into
+; an open file (enabled for writing) in the RAM file system (using the IX handle).
+;
+; The application is responsible for opening/creating a RAM file, then using
+; this library routine to transfer the contents from the File Eprom to the
+; RAM file system, and then finally close the new RAM file.
 ;
 ; IN:
-;    C = slot number containing File Eprom (1, 2 or 3)
-;    IX = handle of file stream (opened as OP_OUT)
-;    BHL = pointer to Eprom File Entry
+;    IX = handle of file stream (opened previosuly as OP_OUT)
+;    BHL = pointer to Eprom File Entry (bits 7,6 of B is the slot mask)
 ;
 ; OUT:
 ;    Fc = 0,
 ;         File Image transferred successfully to RAM file.
 ;
 ;    Fc = 1,
-;         A = RC_ONF, File Eprom or File Entry was not found in slot C
+;         A = RC_ONF, File Eprom or File Entry was not found in slot
 ;         A = RC_xxx, I/O error during saving process.
 ;
 ; Registers changed after return:
@@ -53,46 +55,24 @@
 ;    AF....../.... different
 ;
 ; ------------------------------------------------------------------------
-; Design & programming by Gunther Strube, InterLogic, Dec 1997 - Aug 1998
+; Design & programming by Gunther Strube, Dec 1997-Aug 1998, Sep 2004
 ; ------------------------------------------------------------------------
 ;
 .FileEprFetchFile   PUSH BC
                     PUSH DE
                     PUSH HL
-
-                    PUSH BC
-                    PUSH HL
-                    LD   E,C                      ; preserve slot number
-                    CALL FileEprRequest           ; check for presence of "oz" File Eprom in slot C
-                    LD   D,L
-                    POP  HL
-                    POP  BC
-                    JR   C,no_entry
-                    JR   NZ,no_entry              ; File Eprom not available in slot...
                     
-                    LD   C,E
-                    LD   A,E                      ; preserve slot number in A
-                    CALL FileEprFileSize          ; get size of file in CDE, of entry BHL, slot C
-                    JR   C, no_entry
+                    CALL FileEprFileSize          ; get size of file in CDE, of entry BHL
+                    JR   C, no_entry              ; there was no File Eprom Entry at BHL!
 
                     PUSH BC
                     PUSH DE                       ; preserve size of file (in CDE)
-                    LD   C,A
-                    CALL FileEprFileImage         ; get pointer to file image in BHL, slot C
+                    CALL FileEprFileImage         ; get pointer to file image in BHL
                     EXX
                     POP  DE
                     POP  BC                       ; file size in CDE'
                     EXX
                     JR   C, no_entry
-
-                    LD   A,C
-                    AND  @00000011                ; slots (0), 1, 2 or 3 possible
-                    RRCA
-                    RRCA                          ; converted to Slot mask $40, $80 or $C0
-                    OR   B
-                    LD   B,A                      ; bank in slot C...
-                    RES  7,H                      ; (offset bound into segment 1)
-                    SET  6,H                      ; physical ptr. to file image ready...
 .write_loop
                     EXX                           ; file size = 0 ?
                     LD   A,D
@@ -108,11 +88,6 @@
 .get_block          CALL GetBlockSize             ; get size of block to transfer in HL'
                     CALL TransferFileBlock        ; then transfer block at BHL to RAM file...
                     JR   C,exit_fetch             ; File I/O error occurred, abort...
-
-                    BIT  7,H
-                    JR   Z, write_loop            ; offset still with bank boundary
-                    LD   H,$40                    ; reached boundary...
-                    INC  B                        ; set offset at start of new bank
                     JR   write_loop
 .no_entry
                     SCF
@@ -126,10 +101,11 @@
 
 ; ************************************************************************
 ;
-; Define a block size to transfer to RAM file.
+; Define a block size to transfer to RAM file, which is from the current
+; bank offset and to the 16K bank boundary.
 ;
 ; IN:
-;    HL = offset pointer in bank (linked to segment 1)
+;    HL = offset pointer in bank
 ;
 ; OUT:
 ;    hl = size of block in File Eprom to transfer to RAM file
@@ -143,7 +119,7 @@
                     PUSH HL                       ; preserve BHL pointer...
 
                     EX   DE,HL
-                    LD   HL,$8000                 ; bank boundary...
+                    LD   HL,$4000                 ; 16K bank boundary...
                     CP   A                        ; Fc = 0
                     SBC  HL,DE                    ; HL = <BankSpace>
 
@@ -195,8 +171,6 @@
                     RET
 
 
-
-
 ; ************************************************************************
 ;
 ; Transfer File Block, at (BHL), size hl to file (IX)
@@ -208,14 +182,14 @@
 ; OUT:
 ;    Fc = ?, file I/O status
 ;    A = RC_xxx, if I/O error occurred
-;    HL = points at end of block (or first byte of next block to save)
+;    BHL = updated to point at first byte of next block to save (or EOF)
 ;
 .TransferFileBlock
                     PUSH BC                       ; preserve bank number of ext. address
 
-                    LD   C,1
+                    CALL SafeBHLSegment           ; get a safe segment in C (not this executing segment!) to transfer byte
                     CALL MemDefBank               ; Bind Bank into segment 1 address space
-                    PUSH BC                       ; original bank binding in B
+                    PUSH BC                       ; preserve original bank binding of segment
 
                     EXX
                     PUSH BC
@@ -231,7 +205,18 @@
                     EXX
 
                     POP  BC
-                    CALL MemDefBank               ; restore previous bank binding...
+                    CALL MemDefBank               ; restore previous segment bank binding...
+                    JR   C, err_TransferFileBlock
 
+                    POP  BC
+                    LD   A,H
+                    AND  @00111111
+                    LD   H,A
+                    OR   L
+                    RET  NZ                       ; we're still inside the current bank after block save...
+                    INC  B                        ; set offset at start of new bank
+                    RET
+.err_TransferFileBlock
+                    POP  HL
                     POP  BC
                     RET
