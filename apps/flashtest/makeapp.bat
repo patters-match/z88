fca4496f4304574ebe64a4f758b3cf375524e1d3
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2004-10-26 06:20:37 +0000

    Delete the previous binararies before compiling a new version (makeapp utility will then fail if there were errors in the sources during compilation).

--- apps/flashtest/makeapp.bat
+++ apps/flashtest/makeapp.bat
@@ -1,2 +1,5 @@
-mpm -DDEBUG -t -I..\oz\sysdef -l..\stdlib\standard.lib -b appl.asm
+del flashtest.epr
+del appl.bin
+mpm -t -I..\oz\sysdef -l..\stdlib\standard.lib -b appl.asm
 mpm -b romhdr.asm
+java -jar e:\z88\makeapp.jar flashtest.epr appl.bin 0000 romhdr.bin 3fc0
