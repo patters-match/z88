ab0967a16c0a09270618656e9a8e4ff779b667f6
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2004-11-14 23:01:53 +0000

    delete previous assembler output, then re-assemble, make application card, and finally execute OZvm with FlashStore...

--- apps/flashstore/makeapp.bat
+++ apps/flashstore/makeapp.bat
@@ -1,2 +1,8 @@
+del *.obj
+del *.bin
+del *.map
+del *.epr
 mpm -bv -I..\oz\sysdef -l..\stdlib\standard.lib fsapp
-
+mpm -bv romhdr
+java -jar e:\z88\makeapp.jar flashstore.epr fsapp.bin 3f0000 romhdr.bin 3f3fc0
+java -jar e:\z88\z88.jar ram1 1024 s2 flashstore.epr epr3 512 28f 
