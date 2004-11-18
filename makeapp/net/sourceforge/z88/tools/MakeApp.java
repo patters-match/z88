d11287b2a98f63b7d1ad20fccd2b67f4e4ebfb5d
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2004-11-18 19:56:42 +0000

    size option extended: -szc This will dump the final output of a larger card (32K or more) into 16K bank files, and added with with .63 extension for top bank, .62 for lower bank, etc. Complete Card (size) file is still saved.

--- makeapp/net/sourceforge/z88/tools/MakeApp.java
+++ makeapp/net/sourceforge/z88/tools/MakeApp.java
@@ -39,12 +39,15 @@ public class MakeApp {
 		int appCardSize = 16; 
 		RomBank[] banks = new RomBank[appCardBanks]; 	// the default 16K card container
 		banks[0] = new RomBank(); 						// with a default 16K bank
-		
+		boolean splitBanks = false;
+				
 		try {
 			if (args.length >= 1) {
 				int arg = 0;
 				
-				if (args[0].compareToIgnoreCase("-sz") == 0) {
+				if (args[0].compareToIgnoreCase("-sz") == 0 | args[0].compareToIgnoreCase("-szc") == 0) {
+					if (args[0].endsWith("c") == true) splitBanks = true;
+					
 					appCardSize = Integer.parseInt(args[1], 10);
 					appCardBanks = appCardSize / 16;
 					if (appCardSize % 16 != 0 & appCardBanks != 1 & appCardBanks != 2 & appCardBanks != 4 & 
@@ -58,7 +61,7 @@ public class MakeApp {
 					arg += 2;
 				}
 				
-				RandomAccessFile cardFile = new RandomAccessFile(args[arg++], "rw");
+				String outputFilename = args[arg++];
 				
 				while (arg < args.length) {
 					RandomAccessFile binaryFile =  new RandomAccessFile(args[arg++], "r");
@@ -70,23 +73,40 @@ public class MakeApp {
 					binaryFile.readFully(codeBuffer);
 					banks[bankNo].loadBytes(codeBuffer, offset);
 				}
-								
+
+				RandomAccessFile cardFile = new RandomAccessFile(outputFilename, "rw");
 				for (int b=0; b<appCardBanks; b++) {
 					byte bankDump[] = banks[b].dumpBytes(0, Bank.BANKSIZE); 
 					cardFile.write(bankDump);
 				}
 				cardFile.close();
 				
+				if (splitBanks == true) {
+					// Also dump the binary into 16 bank files...
+					int topBank = 0x3F;
+					for (int b=appCardBanks-1; b>=0; b--) {
+						cardFile = new RandomAccessFile(outputFilename + "." + topBank--, "rw");
+						byte bankDump[] = banks[b].dumpBytes(0, Bank.BANKSIZE); 
+						cardFile.write(bankDump);
+					}					
+				}
 			} else {
-				System.out.println("Syntax: [-sz Size] memdump.file input1.file offset {inputX.file offset}\n");
-				System.out.println("Usage: Load binary files a into default 16K memory bank, and save contents");
-				System.out.println("to a new file. Offsets are specified in hex (truncated to 16K offsets).\n");
+				System.out.println("Syntax: [-sz Size] memdump.file input1.file offset {inputX.file offset}");
+				System.out.println("or");
+				System.out.println("[-szc Size] memdump.file input1.file offset {inputX.file offset}\n");
+				System.out.println("Usage: Load binary files into one or several 16K memory bank, and save it");
+				System.out.println("all to a new file. Offsets are specified in hex (truncated to 16K offsets).\n");
 				System.out.println("Larger application cards is created by optionally specifying size in K, eg.");
 				System.out.println("32 ... up to 1024K. Offsets are then extended with relative bank number,");
 				System.out.println("for example 3fc000 for bank 3f (top), offset 0000 (start of top bank).\n");
-				System.out.println("Example, using default 16K application bank dump:");
-				System.out.println("java -jar makeapp.jar appl.epr code.bin c000 romhdr.bin 3fc0");
-				System.out.println("(load 1st file at 0000, 2nd file at 3fc0, and save 16K bank to appl.epr)");
+				System.out.println("If you need to split a large assembled card into 16 bank on the output,");
+				System.out.println("use the -szc switch, eg. -szc 64 will make both a 64K file and 4 files,");
+				System.out.println("added with .63 for the top bank of the card and downwards.\n");
+				System.out.println("Example, using default 16K application bank dump (java -jar makeapp.jar):");
+				System.out.println("appl.epr code.bin c000 romhdr.bin 3fc0");
+				System.out.println("(load 1st file at 0000, 2nd file at 3fc0, and save 16K bank to appl.epr)\n");
+				System.out.println("Example, using a 32K application bank dump (and separate 16K bank files):");
+				System.out.println("-szc 32 bigappl.epr mth.bin 3e0000 code.bin 3fc000 romhdr.bin 3f3fc0");
 			}
 		} catch (FileNotFoundException e) {
 			System.out.println("Couldn't load file image:\n" + e.getMessage() + "\nprogram terminated.");
