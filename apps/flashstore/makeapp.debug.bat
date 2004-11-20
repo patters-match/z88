ed9e81f231a3dd3fdc1be30ef6ec08b0b1898ba8
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2004-11-20 09:31:03 +0000

    *** empty log message ***

--- apps/flashstore/makeapp.debug.bat
+++ apps/flashstore/makeapp.debug.bat
@@ -1,2 +1,2 @@
 mpm -bv -DDEBUG -I..\oz\sysdef -l..\stdlib\standard.lib fsapp
-java -jar E:\z88\z88.jar s2 E:\z88\debugapp.epr epr3 1024 28f initdebug fsapp.ozvm.cmd
+java -jar E:\z88\z88.jar ram1 1024 s2 E:\z88\debugapp.epr epr3 1024 29f initdebug fsapp.ozvm.cmd
