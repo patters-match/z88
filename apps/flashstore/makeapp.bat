7c4a7caec0946a1d96836a943f25e6a29972caf1
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2004-11-20 09:30:53 +0000

    Use the new crdX feature in OZvm.

--- apps/flashstore/makeapp.bat
+++ apps/flashstore/makeapp.bat
@@ -5,4 +5,4 @@ del *.epr
 mpm -bv -I..\oz\sysdef -l..\stdlib\standard.lib fsapp
 mpm -bv romhdr
 java -jar e:\z88\makeapp.jar flashstore.epr fsapp.bin 3f0000 romhdr.bin 3f3fc0
-java -jar e:\z88\z88.jar ram1 1024 s2 flashstore.epr epr3 512 28f 
+java -jar e:\z88\z88.jar ram1 1024 crd3 1024 29f flashstore.epr
