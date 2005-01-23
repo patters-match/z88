b80ebfa856267a3c2bf63a19d4180f42ef3a372d
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-01-23 14:54:32 +0000

    Implemented basic File Area Header functionality.

---
+++ ozvm/net/sourceforge/z88/datastructures/FileAreaHeader.java
@@ -0,0 +1,75 @@
+/*
+ * FileAreaHeader.java
+ * This	file is	part of	OZvm.
+ *
+ * OZvm	is free	software; you can redistribute it and/or modify	it under the terms of the
+ * GNU General Public License as published by the Free Software	Foundation;
+ * either version 2, or	(at your option) any later version.
+ * OZvm	is distributed in the hope that	it will	be useful, but WITHOUT ANY WARRANTY;
+ * without even	the implied warranty of	MERCHANTABILITY	or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with	OZvm;
+ * see the file	COPYING. If not, write to the
+ * Free	Software Foundation, Inc., 59 Temple Place - Suite 330,	Boston,	MA 02111-1307, USA.
+ *
+ * @author <A HREF="mailto:gbs@users.sourceforge.net">Gunther Strube</A>
+ * $Id$
+ *
+ */
+package net.sourceforge.z88.datastructures;
+
+import net.sourceforge.z88.Memory;
+
+/**
+ * Get File Area Header Information at absolute bank, offset $3FC0-$3FFF.
+ * This class simply reads the available contents of memory, passively.
+ * 
+ * Please use SlotInfo.isFileHeader(bankNo) to ensure that a File header exists
+ * before using this functionality. 
+ */
+public class FileAreaHeader {
+	/** reference to available memory hardware and functionality */
+	private Memory memory = null;
+	
+	private int randomId;
+	private int size;
+	private int subtype;
+	
+	/**
+	 * Read File Header memory content of absolute bankNo, offset $3FC0 - $3FFF.
+	 *  
+	 * @param bankNo
+	 */
+	public FileAreaHeader(int bankNo) {
+		memory = Memory.getInstance();
+					
+		randomId = (memory.getByte(0x3FF8, bankNo) << 24) | 
+					memory.getByte(0x3FF9, bankNo) << 16 |
+					memory.getByte(0x3FFA, bankNo) << 8 | 
+					memory.getByte(0x3FFB, bankNo);
+		
+		size = memory.getByte(0x3FFC, bankNo);  // Size of file area in 16K banks
+		subtype = memory.getByte(0x3FFD, bankNo);
+	}
+	
+	/**
+	 * @return the random ID of the File Header.
+	 */
+	public int getRandomId() {
+		return randomId;
+	}
+	
+	/**
+	 * @return the sub type of the card ($7E for 32K cards, $7C for 128K or larger).
+	 */
+	public int getSubtype() {
+		return subtype;
+	}
+	
+	/**
+	 * @return the size of the file area in 16K banks.
+	 */
+	public int getSize() {
+		return size;
+	}
+}
--- ozvm/net/sourceforge/z88/datastructures/SlotInfo.java
+++ ozvm/net/sourceforge/z88/datastructures/SlotInfo.java
@@ -19,6 +19,10 @@
 
 package net.sourceforge.z88.datastructures;
 
+import net.sourceforge.z88.AmdFlashBank;
+import net.sourceforge.z88.Bank;
+import net.sourceforge.z88.EpromBank;
+import net.sourceforge.z88.IntelFlashBank;
 import net.sourceforge.z88.Memory;
 
 /**
@@ -62,27 +66,63 @@ public class SlotInfo {
 			bankNo = ((slotNo & 3) << 6) | 0x3F;
 		
 		if ( (memory.getByte(0x3FFB, bankNo) == 0x80 | memory.getByte(0x3FFB, bankNo) == 0x81) &
-			memory.getByte(0x3FFE, bankNo) == 'O' &
-			memory.getByte(0x3FFF, bankNo) == 'Z')
+			memory.getByte(0x3FFE, bankNo) == 'O' & memory.getByte(0x3FFF, bankNo) == 'Z')
 			return true;
 		else
 			return false;
 	}
 
 	/**
-	 * Check if specified slot contains a File Card ('oz' watermark).
+	 * Check if there's a File header available at absolute bank, offset $3FC0-$3FFF.
 	 * 
-	 * @return true if Application card is available in slot, otherwise false
+	 * @param bankNo a bank number defining any bank in the external slots ($40 - $FF).
+	 * @return true, if a file header was found, otherwise false. 
 	 */
-	public boolean isFileCard(final int slotno) {
-		// point to watermark in top bank of slot, offset 0x3Fxx
-		int bankNo = ((slotno & 3) << 6) | 0x3F;
-		
-		if (memory.getByte(0x3FF7, bankNo) == 0x01 &
-			memory.getByte(0x3FFE, bankNo) == 'o' &
-			memory.getByte(0x3FFF, bankNo) == 'z')
+	public boolean isFileHeader(final int bankNo) {
+		if (memory.getByte(0x3FF7, (bankNo & 0xFF)) == 0x01 &
+			memory.getByte(0x3FFE, (bankNo & 0xFF)) == 'o' & 
+			memory.getByte(0x3FFF, (bankNo & 0xFF)) == 'z')
 			return true;
 		else
 			return false;
+	}
+
+	/**
+	 * Return absolute bank number (part of specified slot) of found 
+	 * File Header (placed at offset $3FC0-3FFF in bank) in inserted Eprom 
+	 * or Flash Card of specified slot.
+	 *  
+	 * @param slotNo (1-3)
+	 * @return bank number of found file header in slot, or -1 if no file header was found.
+	 */
+	public int getFileHeader(final int slotNo) {
+		// start scan at bottom of card, then upwards...
+		int bankNo = ((slotNo & 3) << 6) ; 
+		Bank bank = memory.getBank(bankNo);
+		
+		if ( (bank instanceof EpromBank == true) | 
+				(bank instanceof AmdFlashBank == true) |
+				(bank instanceof IntelFlashBank == true) ) {
+			
+			for( int topBank=bankNo | (memory.getCardSize(slotNo)-1); 
+				bankNo <= topBank; bankNo++) {
+				if (isFileHeader(bankNo) == true) return bankNo;
+			}
+			return -1;	// reached top of bank, and no file header was found
+		} else {
+			return -1;	// File area not found in Ram cards or empty slots..
+		}		
+	}
+	
+	/**
+	 * Check if specified slot contains a File Card ('oz' watermark at top of card).
+	 * 
+	 * @return true if Application card is available in slot, otherwise false
+	 */
+	public boolean isFileCard(final int slotNo) {
+		// point to watermark in top bank of slot, offset 0x3Fxx
+		int bankNo = ((slotNo & 3) << 6) | 0x3F;
+		
+		return isFileHeader(bankNo);
 	}
 }
