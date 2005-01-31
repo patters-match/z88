ee5994956cfad5e5f4a9b1796b213f0ab2af285a
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-01-31 21:33:11 +0000

    More core functionalities, not yet completed.

--- ozvm/net/sourceforge/z88/filecard/FileArea.java
+++ ozvm/net/sourceforge/z88/filecard/FileArea.java
@@ -18,6 +18,7 @@
  */
 package net.sourceforge.z88.filecard;
 
+import java.util.LinkedList;
 import java.util.Random;
 
 import net.sourceforge.z88.AmdFlashBank;
@@ -26,10 +27,11 @@ import net.sourceforge.z88.EpromBank;
 import net.sourceforge.z88.IntelFlashBank;
 import net.sourceforge.z88.Memory;
 import net.sourceforge.z88.datastructures.ApplicationCardHeader;
+import net.sourceforge.z88.datastructures.FileAreaHeader;
 import net.sourceforge.z88.datastructures.SlotInfo;
 
 /**
- * Management of files in File Area of inserted card in a slot.
+ * Management of files in File Area of inserted card in specified slot (1-3).
  */
 public class FileArea {
 	/** reference to available memory hardware and functionality */
@@ -38,15 +40,157 @@ public class FileArea {
 	/** Utility Class to get slot information */
 	private SlotInfo slotinfo = null;
 
-	public FileArea() {
+	/**
+	 * the slot number of the File area.
+	 */
+	private int slotNumber;
+
+	/**
+	 * The File Header of this File Area. Will be used to validate if another
+	 * File area has been inserted into this slot since last time this object
+	 * was accessed.
+	 */
+	private FileAreaHeader fileAreaHdr;
+
+	/**
+	 * Linked list of available File entries in File Area
+	 */
+	private LinkedList filesList;
+
+	/**
+	 * Scan file area at specified slot
+	 * 
+	 * @param slotNo
+	 */
+	public FileArea(int slotNo) throws FileAreaNotFoundException {
 		memory = Memory.getInstance();
 		slotinfo = SlotInfo.getInstance();
+
+		slotNumber = slotNo;
+		int bankNo = slotinfo.getFileHeaderBank(slotNumber);
+		if (bankNo == -1)
+			throw new FileAreaNotFoundException();
+		else {
+			fileAreaHdr = new FileAreaHeader(bankNo);
+			scanFileArea(); // automatically build the file list...
+		}
 	}
 
 	/**
-	 * Create/reformat a file area in a specified slot. Return <b>true </b> if a
-	 * file area was formatted/created. A file area can only be created on Eprom
-	 * or Flash Cards.
+	 * Scan the memory of the File Area and build a linked list of File entries.
+	 * 
+	 * @throws FileAreaNotFoundException
+	 */
+	public void scanFileArea() throws FileAreaNotFoundException {
+		if (isFileAreaAvailable() == false)
+			throw new FileAreaNotFoundException();
+		else {
+			// dump the old linked list (if any)...
+			filesList = null;
+
+			// start scanning at bottom of card...
+			int fileEntryPtr = (slotNumber << 6) << 16;
+			while (memory.getByte(fileEntryPtr) != 0xFF
+					& memory.getByte(fileEntryPtr) != 0x00) {
+
+				// and create a linked list of FileEntry objects...
+				FileEntry fe = new FileEntry(fileEntryPtr);
+				if (filesList == null)
+					filesList = new LinkedList();
+				filesList.add(fe);
+
+				// point at next File Entry...
+				fileEntryPtr = intToPtr(ptrToInt(fileEntryPtr)
+						+ fe.getHdrLength() + fe.getFileLength());
+			}
+		}
+	}
+
+
+	/**
+	 * Return available free space on File Area. The free area is the number of
+	 * bytes after the last available file and up until the header at the top
+	 * bank of the file area.
+	 * 
+	 * @return free space in bytes
+	 */
+	public int getFreeSpace() throws FileAreaNotFoundException {
+		int freeSpace = 0;
+
+		if (isFileAreaAvailable() == false)
+			throw new FileAreaNotFoundException();
+		else {
+			if (filesList != null) {
+				// there's file entries available, get to the end of the list
+				// and calculate the free space to the file header...
+				
+				FileEntry fe = (FileEntry) filesList.getLast();
+				int freeSpacePtr = intToPtr(ptrToInt(fe.getFileEntryPtr())
+						+ fe.getHdrLength() + fe.getFileLength());
+				
+				int fileHdrPtr = (fileAreaHdr.getBankNo() << 16) | 0x3FC0;
+				freeSpace = ptrToInt(fileHdrPtr) - ptrToInt(freeSpacePtr);  
+			}
+		}
+
+		return freeSpace;
+	}
+
+	/**
+	 * Get pointer to first free space in File Area (where to store a new file).
+	 * 
+	 * @return extended address of free space.
+	 * @throws FileAreaNotFoundException if no file area were found
+	 */
+	private int getFreeSpacePtr() throws FileAreaNotFoundException {
+		int freeSpace = 0;
+
+		if (isFileAreaAvailable() == false)
+			throw new FileAreaNotFoundException();
+		else {
+			if (filesList != null) {
+				// there's file entries available, get to the end of the list
+				FileEntry fe = (FileEntry) filesList.getLast();
+				return intToPtr(ptrToInt(fe.getFileEntryPtr())
+						+ fe.getHdrLength() + fe.getFileLength());
+			} else {
+				// no files available, point at bottom of card...
+				return (slotNumber << 6) << 16;
+			}
+		}
+	}
+	
+	/**
+	 * Store file into File Card/Area.
+	 * 
+	 * @param slotNo
+	 *            of File Card/Area
+	 * @param fileName
+	 *            the filename for the file to be stored (in "oz" filename format).
+	 * @param fileImage
+	 *            the byte image of the file to be stored.
+	 */
+	public void storeFile(String fileName, byte[] fileImage)
+			throws FileAreaNotFoundException, FileAreaExhaustedException {
+	}
+
+	/**
+	 * Check if the file area is still available in the slot (card might have
+	 * been removed/replaced with another card).
+	 * 
+	 * @return true, if a file area was found.
+	 */
+	public boolean isFileAreaAvailable() {
+		if (fileAreaHdr != null)
+			return slotinfo.isFileHeader(fileAreaHdr.getBankNo());
+		else
+			return false;
+	}
+
+	/**
+	 * Create/reformat a file area. Return <b>true </b> if a file area was
+	 * formatted/created. A file area can only be created on Eprom or Flash
+	 * Cards.
 	 * 
 	 * The slot hardware will be evaluated and use the right sub type and
 	 * position of the File Header. For Flash Cards, the header will be
@@ -61,12 +205,11 @@ public class FileArea {
 	 * The complete file area will be formatted with FF's from the bottom of the
 	 * card up until the File Area header.
 	 * 
-	 * @param slotNo
-	 * @return <b>true</b> if file area was formatted/created, otherwise
-	 *         <b>false</b>
+	 * @return <b>true </b> if file area was formatted/created, otherwise
+	 *         <b>false </b>
 	 */
-	public boolean createFileArea(final int slotNo) {
-		int bottomBankNo = slotNo << 6;
+	public boolean createFileArea() {
+		int bottomBankNo = slotNumber << 6;
 
 		// get bottom bank of slot to determine card type...
 		Bank bank = memory.getBank(bottomBankNo);
@@ -74,45 +217,55 @@ public class FileArea {
 				| (bank instanceof AmdFlashBank == true)
 				| (bank instanceof IntelFlashBank == true)) {
 
-			int fileHdrBank = slotinfo.getFileHeaderBank(slotNo);
+			int fileHdrBank = slotinfo.getFileHeaderBank(slotNumber);
 			if (fileHdrBank != -1) {
 				// file header found somewhere on card.
 				// format file area from bottom bank, upwards until header...
 				formatFileArea(bottomBankNo, fileHdrBank);
 			} else {
-				if (slotinfo.isApplicationCard(slotNo) == true) {
-					ApplicationCardHeader appCrdHdr = new ApplicationCardHeader(slotNo);
+				if (slotinfo.isApplicationCard(slotNumber) == true) {
+					ApplicationCardHeader appCrdHdr = new ApplicationCardHeader(
+							slotNumber);
 					if (bank instanceof EpromBank == true) {
-						if (memory.getCardSize(slotNo) == appCrdHdr.getAppAreaSize()) {
-							return false; // there is no room for a file area on Eprom
+						if (memory.getCardSize(slotNumber) == appCrdHdr
+								.getAppAreaSize()) {
+							return false; // there is no room for a file area on
+							// Eprom
 						} else {
 							// format file area just below application area...
-							int topFileAreaBank = bottomBankNo +
-									(memory.getCardSize(slotNo) - appCrdHdr.getAppAreaSize() - 1);
+							int topFileAreaBank = bottomBankNo
+									+ (memory.getCardSize(slotNumber)
+											- appCrdHdr.getAppAreaSize() - 1);
 							formatFileArea(bottomBankNo, topFileAreaBank);
 							createFileHeader(topFileAreaBank);
 						}
 					} else {
-						// check if app area moves into bottom 64K sector... 
-						if (memory.getCardSize(slotNo) - appCrdHdr.getAppAreaSize() < 4)
-							return false; // there is no room for a file area on a flash card...
-						
+						// check if app area moves into bottom 64K sector...
+						if (memory.getCardSize(slotNumber)
+								- appCrdHdr.getAppAreaSize() < 4)
+							return false; // there is no room for a file area on
+						// a flash card...
+
 						// create file area of 64K sector size...
-						int fileAreaSize = memory.getCardSize(slotNo) - appCrdHdr.getAppAreaSize();
+						int fileAreaSize = memory.getCardSize(slotNumber)
+								- appCrdHdr.getAppAreaSize();
 						fileAreaSize -= (fileAreaSize % 4);
-						int topFileAreaBank = bottomBankNo + fileAreaSize-1;
+						int topFileAreaBank = bottomBankNo + fileAreaSize - 1;
 
 						formatFileArea(bottomBankNo, topFileAreaBank);
-						createFileHeader(topFileAreaBank);						
+						createFileHeader(topFileAreaBank);
 					}
 				} else {
 					// empty card, write file header at top of card...
 					formatFileArea(bottomBankNo, bottomBankNo
-							+ memory.getCardSize(slotNo) - 1);
-					createFileHeader(bottomBankNo + memory.getCardSize(slotNo) - 1);
+							+ memory.getCardSize(slotNumber) - 1);
+					createFileHeader(bottomBankNo
+							+ memory.getCardSize(slotNumber) - 1);
 				}
 			}
 
+			filesList = null;
+			
 			return true;
 		} else {
 			// A file area can't be created on a Ram card or in an empty slot...
@@ -150,7 +303,7 @@ public class FileArea {
 	 * @param bankNo
 	 * @return <b>true </b> if file header was created, otherwise <b>false </b>
 	 */
-	public static boolean createFileHeader(int bankNo) {
+	public static boolean createFileHeader(final int bankNo) {
 		Memory memory = Memory.getInstance();
 		Random generator = new Random();
 		int slotNo = (bankNo & 0xC0) >> 6;
@@ -177,7 +330,7 @@ public class FileArea {
 			if ((bank instanceof EpromBank == true)
 					& (memory.getCardSize(slotNo) == 2))
 				memory.setByte(0x3FFD, bankNo, 0x7E); // a 32K Eprom was
-													  // identified...
+			// identified...
 			else
 				memory.setByte(0x3FFD, bankNo, 0x7C); // all other cards get $7C
 
@@ -189,5 +342,33 @@ public class FileArea {
 			// header can't be written to Ram cards or empty slots..
 			return false;
 		}
+	}
+	
+	/**
+	 * Convert the extended address int the current slot to a calculable integer.
+	 * 
+	 * @param extAddress
+	 *            the extended address
+	 * @return integer
+	 */
+	private int ptrToInt(int extAddress) {
+		int bank = (extAddress >> 16) & 0x3F; // no slot info...
+		int offset = extAddress & 0x3FFF;
+
+		return bank * Bank.SIZE + offset;
+	}
+
+	/**
+	 * Convert the calculable integer to an extended address for the current
+	 * slot.
+	 * 
+	 * @param i
+	 * @return extended address
+	 */
+	private int intToPtr(int i) {
+		int bank = i / Bank.SIZE | (slotNumber << 6); // mask slot into address
+		int offset = i % Bank.SIZE;
+
+		return (bank << 16) | offset;
 	}
 }
