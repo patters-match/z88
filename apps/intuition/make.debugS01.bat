ccb608afb3a9b527877914359a54be2e77913b76
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-05-15 11:22:48 +0000

    make scripts to build Intuition for segment 1 & 2.

---
+++ apps/intuition/make.debugS01.bat
@@ -0,0 +1,22 @@
+:: *************************************************************************************
+:: Intuition make script (DOS/Windows) to build executable for segment 1 address space
+:: (C) Gunther Strube (gbs@users.sourceforge.net) 1991-2005
+::
+:: Intuition is free software; you can redistribute it and/or modify it under the terms of the
+:: GNU General Public License as published by the Free Software Foundation;
+:: either version 2, or (at your option) any later version.
+:: Intuition is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+:: without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+:: See the GNU General Public License for more details.
+:: You should have received a copy of the GNU General Public License along with Intuition;
+:: see the file COPYING. If not, write to the
+:: Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+::
+:: $Id$
+::
+:: *************************************************************************************
+
+:: compile Intuition code from scratch
+:: Intuition uses segment 3 for bank switching (Intuition is located at $4000 - segment 1)
+del *.def *.obj *.bin *.map
+..\..\csrc\mpm\mpm -b -DSEGMENT3 -r4000 -I..\oz\sysdef -l..\stdlib\standard.lib @debug
