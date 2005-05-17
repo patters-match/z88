c352a86d369a958a458e6fbddc1397a66266b134
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-05-17 08:35:35 +0000

    crummy code, this Gif stuff!

--- ozvm/net/sourceforge/z88/screen/Gif89Decoder.java
+++ ozvm/net/sourceforge/z88/screen/Gif89Decoder.java
@@ -780,9 +780,6 @@ public class Gif89Decoder {
     lastRect = new Rectangle(ix, iy, iw, ih);
     lastImage = image;
     lastBgColor = bgColor;
-    int dispose = 0;
-    boolean transparency = false;
-    int delay = 0;
     lct = null;
   }
 
