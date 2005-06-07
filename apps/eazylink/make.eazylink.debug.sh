0440ce89fa1dd816df61b8144d6c54ddb2ae8c68
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-06-07 17:17:51 +0000

    Compile scripts for EazyLink debugging (DOS/Windows/Unix/Linux).

---
+++ apps/eazylink/make.eazylink.debug.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# *************************************************************************************
+# EazyLink application make script for Unix/Linux for Intuition debugging
+# (C) Gunther Strube (gbs@users.sourceforge.net) 2005
+#
+# EazyLink is free software; you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation;
+# either version 2, or (at your option) any later version.
+# EazyLink is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with EazyLink;
+# see the file COPYING. If not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# $Id$
+#
+# *************************************************************************************
+
+# compile EazyLink application from scratch
+rm -f *.obj *.bin *.map *.63 *.epr
+../../csrc/mpm/mpm -b -DDEBUGGING -I../oz/sysdef -l../stdlib/standard.lib @eazylink
