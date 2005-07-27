b853c76a376803527dde2a3784c3b51a6b9a379f
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-07-27 23:01:55 +0000

    Updated copyright year (2004 --> 2005).

--- apps/flashstore/romhdr.asm
+++ apps/flashstore/romhdr.asm
@@ -1,8 +1,9 @@
-; **************************************************************************************************
-; This file is part of the Z88 FlashTest application.
+; *************************************************************************************
+; FlashStore
+; (C) Gunther Strube (gbs@users.sourceforge.net) & Thierry Peycru (pek@free.fr), 1997-2005
 ;
-; FlashStore is free software; you can redistribute it and/or modify it under 
-; the terms of the GNU General Public License as published by the Free Software Foundation;
+; FlashStore is free software; you can redistribute it and/or modify it under the terms of the
+; GNU General Public License as published by the Free Software Foundation;
 ; either version 2, or (at your option) any later version.
 ; FlashStore is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
 ; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
@@ -10,10 +11,10 @@
 ; You should have received a copy of the GNU General Public License along with FlashStore;
 ; see the file COPYING. If not, write to the
 ; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-; 
-; $Id$  
 ;
-;***************************************************************************************************
+; $Id$
+;
+; *************************************************************************************
 
 MODULE ROM_HEADER
 
