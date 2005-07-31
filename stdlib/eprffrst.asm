08128941c07a1eb5d002d24adae7e0c0c287d75d
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-07-31 18:50:26 +0000

    Skip NULL file if found on Intel Flash Card.

--- stdlib/eprffrst.asm
+++ stdlib/eprffrst.asm
@@ -3,7 +3,7 @@
 ; **************************************************************************************************
 ; This file is part of the Z88 Standard Library.
 ;
-; The Z88 Standard Library is free software; you can redistribute it and/or modify it under 
+; The Z88 Standard Library is free software; you can redistribute it and/or modify it under
 ; the terms of the GNU General Public License as published by the Free Software Foundation;
 ; either version 2, or (at your option) any later version.
 ; The Z88 Standard Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
@@ -12,12 +12,13 @@
 ; You should have received a copy of the GNU General Public License along with the
 ; Z88 Standard Library; see the file COPYING. If not, write to the
 ; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-; 
-; $Id$  
+;
+; $Id$
 ;
 ;***************************************************************************************************
 
      LIB FileEprRequest
+     LIB FileEprNextFile
      LIB FileEprFileEntryInfo
 
      INCLUDE "error.def"
@@ -28,7 +29,10 @@
 ; Standard Z88 File Eprom Format, including support for sub File Eprom
 ; area in application cards (below application banks in first free 64K boundary)
 ;
-; Return first file entry pointer on Standard File Eprom, inserted in slot C
+; Return first file entry pointer on Standard File Eprom, inserted in slot C.
+;
+; If the NULL file is identified as the first file, it is skipped and the next
+; file entry pointer is automatically returned.
 ;
 ; IN:
 ;    C = slot number containing File Eprom
@@ -38,6 +42,7 @@
 ;         Fz = 1, File Entry marked as deleted
 ;         Fz = 0, File Entry is active.
 ;         BHL = pointer to first file entry in slot (B=00h-FFh, HL=0000h-3FFFh).
+;         (NULL file skipped if found on Intel Flash Card)
 ;
 ;    Fc = 1,
 ;         A = RC_Onf
@@ -48,7 +53,7 @@
 ;    .FB...HL/.... different
 ;
 ; ------------------------------------------------------------------------
-; Design & programming by Gunther Strube, Dec 1997-Aug 1998, Sep 2004
+; Design & programming by Gunther Strube, Dec 1997-Aug 1998, Sep 2004, July 2005
 ; ------------------------------------------------------------------------
 ;
 .FileEprFirstFile   PUSH DE
@@ -68,16 +73,26 @@
                     PUSH BC
                     PUSH HL
                     CALL FileEprFileEntryInfo
+                    LD   A,C                      ; CDE = length of file entry
                     POP  HL
                     POP  BC
                     JR   C, no_entry              ; Ups - no File Entry found...
 
+                    EX   AF,AF'                   ; preserve File status, if it's not the NULL file
+                    OR   D
+                    OR   E                        ; CDE = 0 means that NULL file is present.
+                    JR   NZ, no_null_file
+                    CALL FileEprNextFile          ; ignore NULL file, get pointer to next file (entry)
+                    JR   return_nextfile
+.no_null_file
+                    EX   AF,AF'                   ; get file status back of first file
+.return_nextfile
                     POP  DE                       ; BHL = pointer to first File Entry
                     LD   C,E                      ; original C restored
                     POP  DE
                     LD   A,D                      ; original A restored
                     POP  DE
-                    RET                           
+                    RET
 
 .no_entry           SCF
                     LD   A, RC_Onf                ; "Object not found"
