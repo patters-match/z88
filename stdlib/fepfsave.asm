8851af7b55c5a5e5b468ef1e3994718c5e2ae16c
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-09-06 21:42:05 +0000

    Changed a few comments.

--- stdlib/fepfsave.asm
+++ stdlib/fepfsave.asm
@@ -3,7 +3,7 @@
 ; **************************************************************************************************
 ; This file is part of the Z88 Standard Library.
 ;
-; The Z88 Standard Library is free software; you can redistribute it and/or modify it under 
+; The Z88 Standard Library is free software; you can redistribute it and/or modify it under
 ; the terms of the GNU General Public License as published by the Free Software Foundation;
 ; either version 2, or (at your option) any later version.
 ; The Z88 Standard Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
@@ -12,8 +12,8 @@
 ; You should have received a copy of the GNU General Public License along with the
 ; Z88 Standard Library; see the file COPYING. If not, write to the
 ; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-; 
-; $Id$  
+;
+; $Id$
 ;
 ;***************************************************************************************************
 
@@ -51,16 +51,16 @@
 ; that matches the filename (excl. device). This must be used by a call
 ; to <FlashEprFileDelete>.
 ;
-; Should the actual process of blowing the file image fail, the new 
+; Should the actual process of blowing the file image fail, the new
 ; File Entry will be marked as deleted, if possible.
 ;
-; Important: 
-; INTEL I28Fxxxx series Flash chips require the 12V VPP pin in slot 3 
-; to successfully blow data to the memory chip. If the Flash Eprom card 
-; is inserted in slot 1 or 2, this routine will report a programming failure. 
+; Important:
+; INTEL I28Fxxxx series Flash chips require the 12V VPP pin in slot 3
+; to successfully blow data to the memory chip. If the Flash Eprom card
+; is inserted in slot 1 or 2, this routine will report a programming failure.
 ;
-; It is the responsibility of the application (before using this call) to 
-; evaluate the Flash Memory (using the FlashEprCardId routine) and warn the 
+; It is the responsibility of the application (before using this call) to
+; evaluate the Flash Memory (using the FlashEprCardId routine) and warn the
 ; user that an INTEL Flash Memory Card requires the Z88 slot 3 hardware, so
 ; this type of unnecessary error can be avoided.
 ;
@@ -112,7 +112,7 @@
                     LD   SP,IY
                     PUSH HL                       ; preserve a copy of original SP on return
                     EXX
-                    
+
                     AND  @00000011
                     LD   (IY + CardSlot),A        ; preserve slot number of File Eprom Card
                     LD   (IY + IObuffer),E
@@ -177,7 +177,7 @@
                     POP  BC
                     SBC  HL,BC
                     EX   DE,HL                    ; HL = high word of 32bit free space...
-                    POP  DE
+                    POP  DE                       ; DE = high word of file size
                     SBC  HL,DE
                     JR   C, no_room               ; file size (incl. File Entry Header) > free space...
 
@@ -200,7 +200,7 @@
                     LD   L,(IY + FileEntry)
                     LD   H,(IY + FileEntry+1)
                     LD   B,(IY + FileEntry+2)     ; return pointer to new File Entry...
-.end_filesave       
+.end_filesave
                     EXX
                     POP  HL
                     LD   SP,HL                    ; install original SP
@@ -225,7 +225,7 @@
 ; IN:
 ;    HL = pointer to File Entry
 ;
-.SaveToFlashEpr     
+.SaveToFlashEpr
                     PUSH HL
                     LD   C,(IY + CardSlot)
                     CALL FileEprAllocFilePtr      ; BHL = ptr. to free file space on File Eprom Card
@@ -271,8 +271,8 @@
 ;    ....DE../IXIY same
 ;    AFBC..HL/.... different
 ;
-.SaveFileEntry      PUSH IY                   
-                    PUSH BC                    
+.SaveFileEntry      PUSH IY
+                    PUSH BC
                     LD   A,(DE)                   ; length of filename
                     ADD  A,4+1                    ; total size = length of filename + 1 (file length byte)
                     LD   B,0                      ;              + 4 (32bit file length)
@@ -352,9 +352,9 @@
                     LD   D,(IY + IObuffer+1)      ; Pointer to Buffer Start
                     PUSH DE
                     LD   HL,0
-                    CALL_OZ (Os_Mv)               ; load max. 1K of file into buffer
+                    CALL_OZ (Os_Mv)               ; load max. BC bytes of file into buffer
                     POP  DE
-                    CP   A
+                    CP   A                        ; Fc = 0
                     POP  HL
                     SBC  HL,BC                    ; BC = possible bytes read past EOF (or none)
                     PUSH HL                       ; Fz = 1, indicates EOF!
