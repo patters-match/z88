ad7c0b2a29f5299c875c1fcefe9245289642a93f
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-09-08 19:14:44 +0000

    Bugfix: IY register was not preserved when blowing the actual File Area Header.

--- stdlib/fepstdhd.asm
+++ stdlib/fepstdhd.asm
@@ -3,7 +3,7 @@
 ; **************************************************************************************************
 ; This file is part of the Z88 Standard Library.
 ;
-; The Z88 Standard Library is free software; you can redistribute it and/or modify it under 
+; The Z88 Standard Library is free software; you can redistribute it and/or modify it under
 ; the terms of the GNU General Public License as published by the Free Software Foundation;
 ; either version 2, or (at your option) any later version.
 ; The Z88 Standard Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
@@ -12,8 +12,8 @@
 ; You should have received a copy of the GNU General Public License along with the
 ; Z88 Standard Library; see the file COPYING. If not, write to the
 ; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-; 
-; $Id$  
+;
+; $Id$
 ;
 ;***************************************************************************************************
 
@@ -27,23 +27,23 @@
 ;
 ; Standard Z88 File Eprom Format (using Flash Eprom Card).
 ;
-; Blow File Eprom "oz" header on Flash Eprom, in specified bank (which 
+; Blow File Eprom "oz" header on Flash Eprom, in specified bank (which
 ; is part of the slot that the Flash Memory Card have been inserted into).
 ;
 ; Traditional File Eprom's use the whole card with header in top bank $3F.
 ;
-; Pseudo File Eproms might be part of Application cards below the 
+; Pseudo File Eproms might be part of Application cards below the
 ; reserved application area (as specified by the ROM Front DOR).
 ;
-; Important: 
-; Third generation AMD Flash Memory chips may be programmed in all 
-; available slots (1-3). Only INTEL I28Fxxxx series Flash chips require 
-; the 12V VPP pin in slot 3 to successfully blow the File Eprom header on 
-; the memory chip. If the Flash Eprom card is inserted in slot 1 or 2, 
-; this routine will report a programming failure. 
+; Important:
+; Third generation AMD Flash Memory chips may be programmed in all
+; available slots (1-3). Only INTEL I28Fxxxx series Flash chips require
+; the 12V VPP pin in slot 3 to successfully blow the File Eprom header on
+; the memory chip. If the Flash Eprom card is inserted in slot 1 or 2,
+; this routine will report a programming failure.
 ;
-; It is the responsibility of the application (before using this call) to 
-; evaluate the Flash Memory (using the FlashEprCardId routine) and warn the 
+; It is the responsibility of the application (before using this call) to
+; evaluate the Flash Memory (using the FlashEprCardId routine) and warn the
 ; user that an INTEL Flash Memory Card requires the Z88 slot 3 hardware, so
 ; this type of unnecessary error can be avoided.
 ;
@@ -78,15 +78,15 @@
                     PUSH HL
                     PUSH IX
 
-                    LD   D,B                 
+                    LD   D,B
                     LD   A,B
                     AND  @11000000
                     RLCA
-                    RLCA                     
-                    LD   C,A                      ; get size of card in B of slot C 
+                    RLCA
+                    LD   C,A                      ; get size of card in B of slot C
                     CALL FlashEprCardId
                     JR   C, err_FlashEprStdFileHeader  ; Fc = 1, A = RC error code (Flash Memory not found)
-                    
+
                     LD   HL,0
                     ADD  HL,SP
                     LD   IX,-66
@@ -132,22 +132,24 @@
                     DEC  L
                     JR   Z, whole_card
                     LD   (IX + $3C),L             ; File Eprom area smaller than card size
-                    JR   blow_header                    
-.whole_card         
+                    JR   blow_header
+.whole_card
                     LD   (IX + $3C),C             ; File Eprom area uses whole card
-.blow_header                    
+.blow_header
                     PUSH IX
                     POP  DE                       ; start of File Eprom Header
                     LD   HL, $3FC0                ; blow at address B,$3FC0
                     CALL SafeBHLSegment           ; get a safe segment in C (not this executing segment!) to blow bytes
+                    PUSH IY                       ; (preserve IY)
                     LD   IY, 64                   ; of size
                     CALL FlashEprWriteBlock       ; blow header...
+                    POP  IY
 
                     LD   C,(IX + $3C)             ; return size of File Eprom Area
                     POP  HL
                     LD   SP,HL                    ; restore original Stack Pointer
                     JR   C, err_FlashEprStdFileHeader
- 
+
                     POP  IX                       ; restore registers...
                     POP  HL
                     POP  DE                       ; original AF..
