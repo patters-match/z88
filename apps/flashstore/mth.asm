8137deb9b92537f64327739035c41e5fb0af266f
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-09-09 21:38:41 +0000

    Release V1.8 RC8.

--- apps/flashstore/mth.asm
+++ apps/flashstore/mth.asm
@@ -229,7 +229,7 @@
 ; *******************************************************************************************************************
 ;
 .FlashStoreHelp
-                    DEFM "Release V1.8 RC7, August-September 2005",$7F, $7F
+                    DEFM "Release V1.8 RC8, August-September 2005",$7F, $7F
                     DEFM "Backup/restore files between Rakewell Flash Cards and RAM.", 0
 
 .cmd_sc_help
