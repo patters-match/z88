5acc0b56e322d24e74829e1da891a76708940611
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-09-10 09:06:36 +0000

    Changed email addresses in sources.

--- apps/flashstore/makeapp.bat
+++ apps/flashstore/makeapp.bat
@@ -1,6 +1,6 @@
 :: *************************************************************************************
 :: FlashStore
-:: (C) Gunther Strube (gbs@users.sourceforge.net) & Thierry Peycru (pek@free.fr), 1997-2005
+:: (C) Gunther Strube (gbs@users.sf.net) & Thierry Peycru (pek@users.sf.net), 1997-2005
 ::
 :: FlashStore is free software; you can redistribute it and/or modify it under the terms of the
 :: GNU General Public License as published by the Free Software Foundation;
