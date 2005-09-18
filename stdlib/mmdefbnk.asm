9b604ab2659f89eaa8f4a84cf6c803ec989a650e
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-09-18 14:12:33 +0000

    Shave off three bytes of routine.

--- stdlib/mmdefbnk.asm
+++ stdlib/mmdefbnk.asm
@@ -3,7 +3,7 @@
 ; **************************************************************************************************
 ; This file is part of the Z88 Standard Library.
 ;
-; The Z88 Standard Library is free software; you can redistribute it and/or modify it under 
+; The Z88 Standard Library is free software; you can redistribute it and/or modify it under
 ; the terms of the GNU General Public License as published by the Free Software Foundation;
 ; either version 2, or (at your option) any later version.
 ; The Z88 Standard Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
@@ -12,8 +12,8 @@
 ; You should have received a copy of the GNU General Public License along with the
 ; Z88 Standard Library; see the file COPYING. If not, write to the
 ; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-; 
-; $Id$  
+;
+; $Id$
 ;
 ;***************************************************************************************************
 
@@ -51,10 +51,7 @@
 
                     POP  BC
                     LD   B,A                 ; return previous bank binding
-                    POP  AF
-                    POP  HL
-                    RET
-.already_bound      
+.already_bound
                     POP  AF
                     POP  HL
                     RET
