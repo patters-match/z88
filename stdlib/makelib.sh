a7fdfb3fb80844d6bd160c3e86a074aedfc03e3a
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-09-28 20:08:49 +0000

    Edited the compile script for DOS/Windows to use the paths of the new project structure. Also added a similar shell script for Unix/Linux platforms.

---
+++ stdlib/makelib.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# *************************************************************************************
+# Z88 Standard Library Makefile for Unix/Linux
+# (C) Gunther Strube (gbs@users.sf.net) 1991-2005
+#
+# This is free software; you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation;
+# either version 2, or (at your option) any later version.
+# The software is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with this software;
+# see the file COPYING. If not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# $Id$
+#
+# *************************************************************************************
+
+rm -f standard.lib
+
+# Compile library routines into .obj files and generate the standard.lib file
+# (to be used by other applications that needs to statically link routines from this library)
+#
+# The standard library is located in /stdlib
+# The Z80 assembler is located in /tools/mpm
+# The OZ Manifests are located in /oz/sysdef
+
+../tools/mpm/mpm -I../oz/sysdef -xstandard.lib @standard
