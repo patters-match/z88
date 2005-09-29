9d06ebab6fb647f1eccd543055324402c6efd543
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-09-29 08:32:41 +0000

    Added compile scripts for EP-Fetch2 popdown.

---
+++ z88apps/epfetch/make.epfetch.bat
@@ -0,0 +1,22 @@
+:: *************************************************************************************
+:: EP-Fetch2 application make script for DOS/Windows
+::
+:: EP-Fetch2 is free software; you can redistribute it and/or modify it under the terms of the
+:: GNU General Public License as published by the Free Software Foundation;
+:: either version 2, or (at your option) any later version.
+:: EP-Fetch2 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+:: without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+:: See the GNU General Public License for more details.
+:: You should have received a copy of the GNU General Public License along with EP-Fetch2;
+:: see the file COPYING. If not, write to the
+:: Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+::
+:: $Id$
+::
+:: *************************************************************************************
+
+:: compile EP-Fetch2 application from scratch
+:: (this compile script is located in /z88apps/epfetch)
+
+del *.obj *.bin *.map
+..\..\tools\mpm\mpm -b -I..\..\oz\sysdef epfetch2
