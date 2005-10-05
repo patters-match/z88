5521a9786cd0e45b6d763bd9b7da60d3fd318b23
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-10-05 18:01:43 +0000

    Implemented two new libraries that disables and enables the interrupt flow from the Blink. These routines are good replacements for the usual DI/EI instructions (and OZ's own CALL OZ_DI/OZ_EI), since non-maskable interrupts also can be blocked (ie. bat-low interrupts).

---
+++ stdlib/enablnkint.asm
@@ -0,0 +1,45 @@
+     XLIB EnableBlinkInt
+
+; **************************************************************************************************
+; This file is part of the Z88 Standard Library.
+;
+; The Z88 Standard Library is free software; you can redistribute it and/or modify it under
+; the terms of the GNU General Public License as published by the Free Software Foundation;
+; either version 2, or (at your option) any later version.
+; The Z88 Standard Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+; See the GNU General Public License for more details.
+; You should have received a copy of the GNU General Public License along with the
+; Z88 Standard Library; see the file COPYING. If not, write to the
+; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;
+; $Id$
+;
+;***************************************************************************************************
+
+     INCLUDE "blink.def"
+
+
+; ***************************************************************************
+;
+; Enable interrupts getting out of Blink
+;
+; IN:
+;    -
+;
+; OUT:
+;    -
+;
+; Registers changed after return:
+;    AFBCDEHL/IXIY same
+;    ......../.... different
+;
+.EnableBlinkInt     push af
+                    push hl
+                    ld   hl,BLSC_INT
+                    set  BB_INTGINT,(hl)          ; (update soft copy first)
+                    ld   a,(hl)
+                    out  (BL_INT),a               ; interrupts allowed to get out of blink (GINT = 0)
+                    pop  hl
+                    pop  af
+                    RET
