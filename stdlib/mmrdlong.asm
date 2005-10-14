da5d45516b83dd10241a952dd6c0fe8736707130
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-10-14 17:13:31 +0000

    New functionality (backwards compatible with old API): Read/write long integer at local address, if B(in) = 0 (using the standard convention for many other OZ system calls).

--- stdlib/mmrdlong.asm
+++ stdlib/mmrdlong.asm
@@ -3,7 +3,7 @@
 ; **************************************************************************************************
 ; This file is part of the Z88 Standard Library.
 ;
-; The Z88 Standard Library is free software; you can redistribute it and/or modify it under 
+; The Z88 Standard Library is free software; you can redistribute it and/or modify it under
 ; the terms of the GNU General Public License as published by the Free Software Foundation;
 ; either version 2, or (at your option) any later version.
 ; The Z88 Standard Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
@@ -12,24 +12,27 @@
 ; You should have received a copy of the GNU General Public License along with the
 ; Z88 Standard Library; see the file COPYING. If not, write to the
 ; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-; 
-; $Id$  
+;
+; $Id$
 ;
 ;***************************************************************************************************
 
      LIB SafeBHLSegment, MemDefBank
 
 
-; ******************************************************************************
+;***************************************************************************************************
 ;
-; Read long integer (in debc) at pointer in BHL,A.
+; Read long integer in de'bc' at base record pointer in BHL, offset A.
+;
+; If B<>0, the long integer is read from extended address.
+; If B=0, the long integer is read from local address space.
 ;
 ;    Register affected on return:
 ;         A.BCDEHL/IXIY .......  same
 ;         .F....../.... afbcdehl different
 ;
 ; ----------------------------------------------------------------------
-; Design & programming by Gunther Strube, 1997, Sept. 2004
+; Design & programming by Gunther Strube, 1997, Sep 2004, Oct 2005
 ; ----------------------------------------------------------------------
 ;
 .MemReadLong        PUSH HL
@@ -40,10 +43,11 @@
                     LD   E,A
                     ADD  HL,DE                    ; add offset to pointer
 
-                    CALL SafeBHLSegment           ; get a safe segment (not this executing segment!)
-                                                  ; C = Safe MS_Sx segment
-                                                  ; HL points into segment C
-                    CALL MemDefBank               ; page in bank temporarily
+                    INC  B
+                    DEC  B                        ; B<>0, then bind ext. address into local address space
+                    CALL NZ,SafeBHLSegment        ; get a safe non-executing segment
+                    CALL NZ,MemDefBank            ; page in bank at C = Safe MS_Sx segment,
+
                     PUSH HL
                     EXX
                     POP  HL
@@ -55,7 +59,8 @@
                     INC  HL
                     LD   D,(HL)
                     EXX
-                    CALL MemDefBank               ; restore prev. binding
+
+                    CALL NZ,MemDefBank             ; restore previous bank binding
 
                     POP  BC
                     POP  DE
--- stdlib/mmwrlong.asm
+++ stdlib/mmwrlong.asm
@@ -3,7 +3,7 @@
 ; **************************************************************************************************
 ; This file is part of the Z88 Standard Library.
 ;
-; The Z88 Standard Library is free software; you can redistribute it and/or modify it under 
+; The Z88 Standard Library is free software; you can redistribute it and/or modify it under
 ; the terms of the GNU General Public License as published by the Free Software Foundation;
 ; either version 2, or (at your option) any later version.
 ; The Z88 Standard Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
@@ -12,27 +12,30 @@
 ; You should have received a copy of the GNU General Public License along with the
 ; Z88 Standard Library; see the file COPYING. If not, write to the
 ; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-; 
-; $Id$  
+;
+; $Id$
 ;
 ;***************************************************************************************************
 
      LIB SafeBHLSegment, MemDefBank
 
 
-; ******************************************************************************
+;***************************************************************************************************
 ;
-; Set long integer in debc (alternate), at pointer in BHL,A.
+; Set long integer in de'bc' at base record pointer in BHL, offset A.
+;
+; If B<>0, the long integer is written to extended address.
+; If B=0, the long integer is written to local address space.
 ;
 ;    Register affected on return:
 ;         A.BCDEHL/IXIY same
 ;         .F....../.... different
 ;
 ; ----------------------------------------------------------------------
-; Design & programming by Gunther Strube, 1995-98, Sept. 2004
+; Design & programming by Gunther Strube, 1995-98, Sep 2004, Oct 2005
 ; ----------------------------------------------------------------------
 ;
-.MemWriteLong       
+.MemWriteLong
                     PUSH BC
                     PUSH DE
                     PUSH HL
@@ -41,10 +44,11 @@
                     LD   E,A
                     ADD  HL,DE                    ; add offset to pointer
 
-                    CALL SafeBHLSegment           ; get a safe segment (not this executing segment!)
-                                                  ; C = Safe MS_Sx segment
-                                                  ; HL points into segment C
-                    CALL MemDefBank               ; page in bank temporarily
+                    INC  B
+                    DEC  B                        ; B<>0, then bind ext. address into local address space
+                    CALL NZ,SafeBHLSegment        ; get a safe non-executing segment
+                    CALL NZ,MemDefBank            ; page in bank at C = Safe MS_Sx segment,
+
                     PUSH HL
                     EXX
                     POP  HL
@@ -56,7 +60,8 @@
                     INC  HL
                     LD   (HL),D
                     EXX
-                    CALL MemDefBank               ; restore prev. binding (original B register)
+
+                    CALL NZ,MemDefBank            ; restore previous bank binding
 
                     POP  HL
                     POP  DE
