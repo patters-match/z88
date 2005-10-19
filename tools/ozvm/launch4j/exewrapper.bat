bb73adaacf741e285a69285fc4b031599d6a8abc
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-10-19 19:49:48 +0000

    Added a script for Windows to compile OZvm from scratch using the Java Development Kit 1.4 or later (so developers just need to install the JDK from Sun, then run this script to get an executable JAR file - no need to install a complete IDE like Eclipse or NetBeans!).
    Added some more comments to the Launch4J EXE wrapper compile script.

--- tools/ozvm/launch4j/exewrapper.bat
+++ tools/ozvm/launch4j/exewrapper.bat
@@ -1,12 +1,32 @@
-:: --------------------------------------------------------------------------
+:: *************************************************************************************
+:: Launch4J compile script to make a Windows EXE-cutable of the z88.jar (OZvm) file.
+::
+:: (C) Gunther Strube (gbs@users.sf.net) 2005
+::
+:: OZvm is free software; you can redistribute it and/or modify it under the terms of the
+:: GNU General Public License as published by the Free Software Foundation;
+:: either version 2, or (at your option) any later version.
+:: OZvm is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+:: without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+:: See the GNU General Public License for more details.
+:: You should have received a copy of the GNU General Public License along with OZvm;
+:: see the file COPYING. If not, write to the
+:: Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+::
+:: $Id$
+::
+:: ------------------------------------------------------------------------------------
 :: This BAT file automatically creates the EXE wrapper of the z88.jar file,
-:: located in the parent directory. Call this BAT file in the same directory
-:: as the launch4j-ozvm.xml configuration file.
+:: located in the parent directory. Call this BAT file in the \launch4j directory
+:: (same as the launch4j-ozvm.xml configuration file).
 ::
 :: Before calling this script, add install directory of Launch4Jc.exe to
 :: your PATH environment variable. The "launch4j-ozvm.xml" must be used
 :: with Launch4J 2.0RC3 or newer.
-:: --------------------------------------------------------------------------
+::
+:: Launch4J can be downloaded from http://launch4j.sourceforge.net/
+::
+:: *************************************************************************************
 
 :: --------------------------------------------------------------------------
 :: some trickery to get current directory into a variable
@@ -22,6 +42,6 @@ FOR %%c IN (CALL DEL) DO %%c %temp%.\setdir.bat
 :: --------------------------------------------------------------------------
 
 :: --------------------------------------------------------------------------
-:: create the EXE wrapper in parent directory
+:: create the EXE wrapper in parent directory (the ozvm directory)
 launch4jc.exe %cd%\launch4j-ozvm.xml
 :: --------------------------------------------------------------------------
---
+++ tools/ozvm/makejar.bat
@@ -0,0 +1,57 @@
+:: *************************************************************************************
+:: Windows compile script for OZvm - the Z88 emulator.
+:: (C) Gunther Strube (gbs@users.sf.net) 2003-2005
+::
+:: OZvm is free software; you can redistribute it and/or modify it under the terms of the
+:: GNU General Public License as published by the Free Software Foundation;
+:: either version 2, or (at your option) any later version.
+:: OZvm is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+:: without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+:: See the GNU General Public License for more details.
+:: You should have received a copy of the GNU General Public License along with OZvm;
+:: see the file COPYING. If not, write to the
+:: Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+::
+:: $Id$
+::
+:: ------------------------------------------------------------------------------------
+:: Before executing this script, a Java Development Kit 1.4 or later must have been
+:: installed and the PATH environment variable set to the <jdk install>\bin folder.
+:: (Control Panel -> "System" -> Advanced -> System Variables -> Click on "Path", then Edit)
+::
+:: To test the availablity of command line java compiler, just type "javac -version".
+:: This will display the version and the compile options to your console window.
+::
+:: *************************************************************************************
+@echo off
+
+:: compile the java classes of the project
+echo compiling java classes
+javac -nowarn -g:none com/imagero/util/*.java
+javac -nowarn -g:none net/sourceforge/z88/datastructures/*.java
+javac -nowarn -g:none net/sourceforge/z88/filecard/*.java
+javac -nowarn -g:none net/sourceforge/z88/screen/*.java
+javac -nowarn -g:none net/sourceforge/z88/*.java
+
+:: create a temporary dir for files to be included in the executable JAR file
+mkdir ..\ozvm-builddir >nul
+
+: copy the all the application files (without hidden files)
+echo building executable jar
+xcopy com ..\ozvm-builddir\com /S /Y /I /Q >nul
+xcopy net ..\ozvm-builddir\net /S /Y /I /Q >nul
+xcopy pixel ..\ozvm-builddir\pixel /S /Y /I /Q >nul
+xcopy .\ozvm-manual.html ..\ozvm-builddir /Q >nul
+xcopy .\Z88.rom ..\ozvm-builddir /Q >nul
+
+:: JAR file only needs (compiled) class files
+del /S /Q ..\ozvm-builddir\*.java >nul
+
+:: finally, build the executable jar
+cd ..\ozvm-builddir >nul
+jar cfm ../ozvm/z88.jar ../ozvm/META-INF/MANIFEST.MF . >nul
+
+:: clean up the temp build stuff
+cd ..\ozvm
+rmdir /S /Q ..\ozvm-builddir >nul
+echo z88.jar completed.
