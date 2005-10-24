4fcf7b2257185183191145627ddb27356d85dec4
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-10-24 23:08:26 +0000

    Added Unix/Linux compile script for BBC BASIC program version.

---
+++ z88apps/romupdate/makebas.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# *************************************************************************************
+# RomUpdate - BBC BASIC compile script
+# (C) Gunther Strube (gbs@users.sf.net) 2005
+#
+# RomUpdate is free software; you can redistribute it and/or modify it under the terms of the
+# GNU General Public License as published by the Free Software Foundation;
+# either version 2, or (at your option) any later version.
+# RomUpdate is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with RomUpdate;
+# see the file COPYING. If not, write to the
+# Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+#
+# $Id$
+#
+# *************************************************************************************
+
+del *.obj *.bin *.bas *.map
+../../tools/mpm/mpm -b -I../../oz/sysdef -l../../stdlib/standard.lib @romupdate.popdown.prj
+
+# this is actually to be run as a BBC BASIC program on the Z88
+mv bbcbasic.bin romupdate.bas
