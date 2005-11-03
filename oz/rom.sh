fda7c12007f45506e4fe7b6845b9bae83d3beeec
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-11-03 19:55:33 +0000

    combine the compiled banks into 128K rom binary using Unix cat command

--- oz/rom.sh
+++ oz/rom.sh
@@ -11,4 +11,4 @@ cd ../bank6
 ./bank6.sh
 cd ..
 
-cp -f kernel.bn1 bank1/bank1.bin bank2/bank2.bin bank3/bank3.bin banks45/pipedrm.dat bank6/bank6.bin kernel.bn0 > oz.bin
+cat kernel.bn1 bank1/bank1.bin bank2/bank2.bin bank3/bank3.bin banks45/pipedrm.dat bank6/bank6.bin kernel.bn0 > oz.bin
