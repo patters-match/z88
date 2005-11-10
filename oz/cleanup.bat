7374f39f625c2b530c9367d33eaf6eec2dd1500c
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-11-10 15:28:33 +0000

    also .lst files.

--- oz/cleanup.bat
+++ oz/cleanup.bat
@@ -4,21 +4,21 @@
 del /S /Q kernel.bin kernel.map kernel.obj kernel.bn? 2>nul >nul
 
 cd bank0
-del /S /Q *.obj *.bin *.map *.err *.wrn 2>nul >nul
+del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
 
 cd ..\bank1
-del /S /Q *.obj *.bin *.map *.err *.wrn 2>nul >nul
+del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
 
 cd ..\bank2
-del /S /Q *.obj *.bin *.map *.err *.wrn 2>nul >nul
+del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
 
 cd ..\bank3
-del /S /Q *.obj *.bin *.map *.err *.wrn 2>nul >nul
+del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
 
 cd ..\bank6
-del /S /Q *.obj *.bin *.map *.err *.wrn 2>nul >nul
+del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
 
-cd ..\bank7 
-del /S /Q *.obj *.bin *.map *.err *.wrn 2>nul >nul
+cd ..\bank7
+del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
 
 cd ..
