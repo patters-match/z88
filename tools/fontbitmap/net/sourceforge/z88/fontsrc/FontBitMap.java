a1cff4e5bcbb1a99f48ddbba9428fe2d21f5d180
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-11-10 21:54:01 +0000

    More infra structure improvement of the utility.

--- tools/fontbitmap/net/sourceforge/z88/fontsrc/FontBitMap.java
+++ tools/fontbitmap/net/sourceforge/z88/fontsrc/FontBitMap.java
@@ -1,3 +1,21 @@
+/*
+ * FontBitMap - Z88 Font source code generator
+ * (C) Copyright Gunther Strube (gbs@users.sf.net), 2005
+ *
+ * FontBitMap is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation;
+ * either version 2, or (at your option) any later version.
+ * FontBitMap is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along with FontBitMap;
+ * see the file COPYING. If not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ *
+ * @author <A HREF="mailto:gbs@users.sourceforge.net">Gunther Strube</A>
+ * $Id$
+ */
+
 package net.sourceforge.z88.fontsrc;
 
 import java.io.BufferedWriter;
@@ -12,20 +30,25 @@ public class FontBitMap {
 	private static final char[] hexcodes = { '0', '1', '2', '3', '4', '5', '6',
 			'7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F' };
 
-	private File fontFile;
-
+	private File romFile;
 	private File asmFile;
+	private File fontBitMapFile;
+	private File tokenTableFile;
 
 	private BufferedWriter bwAsmFile;
 
 	private byte[] fontBitMap;
-
+	private byte[] tokenTable; 
+	
 	/** constructor, define input & output filenames */
-	public FontBitMap(String fontFilename) {
-		fontFile = new File(fontFilename);
-		asmFile = new File(fontFilename + ".asm");
+	public FontBitMap(String romFilename) {
+		romFile = new File(romFilename);
+		asmFile = new File(romFilename + ".asm");
+		fontBitMapFile = new File(romFilename + ".dat");
+		tokenTableFile = new File(romFilename + ".tkt");
 	}
 
+	
 	/**
 	 * Return Binary 8bit string in 01010101b zero prefixed format.
 	 * 
@@ -50,6 +73,7 @@ public class FontBitMap {
 		return binString.toString();
 	}
 
+	
 	/**
 	 * Return Hex 16bit address string in XXXXh zero prefixed format.
 	 * 
@@ -72,7 +96,8 @@ public class FontBitMap {
 		return hexString.toString();
 	}
 
-	/** display the character as a comment and a defb sequence */
+	
+	/** write the character as a comment and a defb sequence to the source code file */
 	private void dumpCharMatrix(int offset) {
 		try {
 			bwAsmFile.write("\n\n; Char entry $" + addrToHex(offset / 8, false)
@@ -100,12 +125,57 @@ public class FontBitMap {
 		}
 	}
 
-	private void scanFontFile() {
+	
+	/** dump the font bitmap in the ROM as a binary file */
+	private void dumpFontBitMap() throws IOException {	
+		// dump font bit map as separate file..
+		fontBitMapFile.delete();
+		RandomAccessFile rafFontBitMap = new RandomAccessFile(fontBitMapFile, "rw");
+		rafFontBitMap.write(fontBitMap);
+		rafFontBitMap.close();
+	}
+	
+	
+	/** Dump the embedded Token Table in font bitmap as stand-alone binary file 
+	 * @throws IOException */
+	private void dumpTokenTable() throws IOException {
+		int index=0;
+
+		// extract the token table, embedded inside the font bitmap in
+		// bit 7,6 of each byte.
+		tokenTable = new byte[fontBitMap.length/4];
+		for (int b=0, n=fontBitMap.length; b<n; b+=4) {
+			int tbyte = 0;
+			for (int bits=b; bits<(b+4); bits++) {
+				tbyte = (tbyte << 2) | ((fontBitMap[bits] & 0xc0) >>> 6); 
+			}
+			tokenTable[index++] = (byte) tbyte;
+		}
+		
+		int tokenCount = tokenTable[1] & 0xff;			
+		int endTokenPtrOffset = 2 + tokenCount*2;
+		int sizeOfTokenTable = (tokenTable[endTokenPtrOffset+1] & 0xff) * 256 + (tokenTable[endTokenPtrOffset] & 0xff);
+		// System.out.println(addrToHex(tokenCount,true) + "," + addrToHex(endTokenPtrOffset,true) + "," + addrToHex(sizeOfTokenTable,true));
+		
+		// Raw dump of the extracted embedded token table inside font bit map 
+		tokenTableFile.delete();
+		RandomAccessFile rafTokenTable = new RandomAccessFile(tokenTableFile, "rw");
+		rafTokenTable.write(tokenTable, 0, sizeOfTokenTable);
+		rafTokenTable.close();		
+	}
+	
+	
+	/** 
+	 * Scan the font bitmap in the ROM, generate source code for 
+	 * font bitmaps and stand-alone token table,
+	 * dump the font bitmap and token table as binary files.  
+	 */
+	private void scanFontBitMap() {
 		try {
-			RandomAccessFile rafFontBitMap = new RandomAccessFile(fontFile, "r");
-			byte[] romImage = new byte[(int) rafFontBitMap.length()];
-			rafFontBitMap.readFully(romImage);
-			rafFontBitMap.close();
+			RandomAccessFile rafRomImage = new RandomAccessFile(romFile, "r");
+			byte[] romImage = new byte[(int) rafRomImage.length()];
+			rafRomImage.readFully(romImage);
+			rafRomImage.close();
 			fontBitMap = new byte[0xF00]; // size of lores1 + hires1 Font Bit Map
 
 			// copy font bit map at start of bank 7 from ROM file image
@@ -113,7 +183,13 @@ public class FontBitMap {
 			for (int b = 0x1c000; b < (0x1c000 + 0xf00); b++)
 				fontBitMap[index++] = romImage[b];
 
-			// create a text file for output.
+			// dump the Font Bitmap as stand-alone file image (4K)
+			dumpFontBitMap();
+			
+			// dump the embedded Token Table in font bitmap as stand-alone file image (1K)
+			dumpTokenTable();
+
+			// create source code as text file...
 			asmFile.delete();
 			bwAsmFile = new BufferedWriter(new FileWriter(asmFile));
 
@@ -121,38 +197,7 @@ public class FontBitMap {
 				dumpCharMatrix(f);
 			}
 			bwAsmFile.close();
-
-			// dump font bit map as separate file..
-			File fontBitMapFile = new File(fontFile + ".dat");
-			fontBitMapFile.delete();
-			rafFontBitMap = new RandomAccessFile(fontBitMapFile, "rw");
-			rafFontBitMap.write(fontBitMap);
-			rafFontBitMap.close();
-
-			// extract the token table, embedded inside the font bitmap in
-			// bit 7,6 of each byte.
-			byte[] tokentable = new byte[fontBitMap.length/4];
-			index=0;
-			for (int b=0, n=fontBitMap.length; b<n; b+=4) {
-				int tbyte = 0;
-				for (int bits=b; bits<(b+4); bits++) {
-					tbyte = (tbyte << 2) | ((fontBitMap[bits] & 0xc0) >>> 6); 
-				}
-				tokentable[index++] = (byte) tbyte;
-			}
-			
-			int tokenCount = tokentable[1] & 0xff;			
-			int endTokenPtrOffset = 2 + tokenCount*2;
-			int sizeOfTokenTable = (tokentable[endTokenPtrOffset+1] & 0xff) * 256 + (tokentable[endTokenPtrOffset] & 0xff);
-			// System.out.println(addrToHex(tokenCount,true) + "," + addrToHex(endTokenPtrOffset,true) + "," + addrToHex(sizeOfTokenTable,true));
-			
-			// Raw dump of the extracted embedded token table inside font bit map 
-			File tokenTableFile = new File(fontFile + ".tkt");
-			tokenTableFile.delete();
-			RandomAccessFile rafTokenTable = new RandomAccessFile(tokenTableFile, "rw");
-			rafTokenTable.write(tokentable, 0, sizeOfTokenTable);
-			rafTokenTable.close();
-			
+						
 		} catch (FileNotFoundException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
@@ -160,18 +205,20 @@ public class FontBitMap {
 		}
 	}
 
+	
 	/**
-	 * open specified filename, scan Z88 font bitmap and generate source code
+	 * Open specified Z88 ROM, scan Z88 font bitmap and generate source code
+	 * for fonts and token table.
 	 * 
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		if (args.length == 0) {
 			System.out
-					.println("Usage java -jar Z88FontSources <Z88 ROM filename>");
+					.println("Usage: java -jar fontbitmap.jar <Z88 ROM filename>");
 		} else {
 			FontBitMap zfs = new FontBitMap(args[0]);
-			zfs.scanFontFile();
+			zfs.scanFontBitMap();
 		}
 	}
 }
