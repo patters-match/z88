5b1480c654bec98b44c8bf97b37a082611c9d502
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-11-18 11:52:22 +0000

    First draft of new EXTCALL functionality for OZ.

---
+++ oz/bank7/extcall.asm
@@ -0,0 +1,80 @@
+module extcall
+
+; **************************************************************************************************
+; This file is part of the Z88 operating system, OZ
+;
+; OZ is free software; you can redistribute it and/or modify it under
+; the terms of the GNU General Public License as published by the Free Software Foundation;
+; either version 2, or (at your option) any later version.
+; OZ is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+; See the GNU General Public License for more details.
+; You should have received a copy of the GNU General Public License along with OZ;
+; see the file COPYING. If not, write to the
+; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+;
+; $Id$
+;
+;***************************************************************************************************
+
+
+;***************************************************************************************************
+; EXTCALL - 24bit Call Subroutine in external bank
+;
+; This routine is executed by the RST 10H vector. Both RST 10H and this routine is located
+; in LOWRAM. The 24bit address is available in low byte - high byte order following the RST 10H
+; instruction.
+;
+; Example in Z80 assembler: RST 10H, $FEC000, execute code in bank $FE at address $C000.
+; (the instruction opcode sequence in memory is $D7, $00, $C0, $FE)
+;
+; The ExtCall does not destroy any registers call arguments or return values. ExtCall
+; is to be regarded as a normal CALL instruction, but for 24bit address range.
+;
+; ----------------------------------------------------------------------
+; Design & programming by Gunther Strube, 2005
+; ----------------------------------------------------------------------
+;
+.ExtCall
+        ex      (sp),hl                         ; HL points at 24bit address argument (original HL on stack
+        push    af
+        call    PreserveBCDE                    ; preserve original BC, DE registers temporarily
+        ld      e,(hl)                          ; low byte of call address
+        inc     hl
+        ld      d,(hl)                          ; high byte of call address
+        inc     hl
+        ld      b,(hl)                          ; bank number of 24bit address
+        inc     hl                              ; correct RETurn address
+        ld      c,d
+        rlc     c
+        rlc     c                               ; segment specifier (in bits 0,1 - remaining bits are stripped by RST 30H)
+        rst     OS_MPB                          ; bind in bank of 24bit address (returned B = old bank binding)
+
+        pop     af
+        ex      (sp),hl                         ; new RETurn address points at instruction after 24bit call address
+        ld      bc,restore_bank_binding         ; (HL now original caller value)
+        push    bc                              ; call'ed subroutine RETurns to restore the old bank binding before
+        push    de                              ; actually returning from ExtCall...
+        call    RestoreBCDE
+        ret                                     ; CALL address to subroutine in bound bank
+
+.restore_bank_binding                           ; when subroutine executes RET, it is returned here...
+        call    PreserveBC
+        pop     bc                              ; old bank binding...
+        rst     OS_MPB                          ; restore previous bank binding
+        call    RestoreBC
+        ret
+
+.PreserveBCDE
+        ld      (regs+2),de
+.PreserveBC
+        ld      (regs),bc
+        ret
+.RestoreBCDE
+        ld      de,(regs+2)
+.RestoreBC
+        ld      bc,(regs)
+        ret
+.regs
+        defw    0                               ; storage space for original BC register in LOWRAM
+        defw    0                               ; storage space for original DE register in LOWRAM
