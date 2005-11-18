3006a24d2967219f1f5b6c9cd4a81de49fef7a3f
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-11-18 12:15:37 +0000

    restriction comments added.

--- oz/bank7/extcall.asm
+++ oz/bank7/extcall.asm
@@ -31,13 +31,16 @@ module extcall
 ; The ExtCall does not destroy any registers call arguments or return values. ExtCall
 ; is to be regarded as a normal CALL instruction, but for 24bit address range.
 ;
+; RESTRICTION:
+; This routine cannot be used in Blink NMI or INT service routines because it would corrupt
+; the static storage variables at (regs) while ExtCall is being executed by normal processor.
+;
 ; ----------------------------------------------------------------------
 ; Design & programming by Gunther Strube, 2005
 ; ----------------------------------------------------------------------
 ;
 .ExtCall
         ex      (sp),hl                         ; HL points at 24bit address argument (original HL on stack
-        push    af
         call    PreserveBCDE                    ; preserve original BC, DE registers temporarily
         ld      e,(hl)                          ; low byte of call address
         inc     hl
@@ -46,13 +49,14 @@ module extcall
         ld      b,(hl)                          ; bank number of 24bit address
         inc     hl                              ; correct RETurn address
         ld      c,d
+        push    af                              ; preserve flags...
         rlc     c
         rlc     c                               ; segment specifier (in bits 0,1 - remaining bits are stripped by RST 30H)
+        pop     af
         rst     OS_MPB                          ; bind in bank of 24bit address (returned B = old bank binding)
 
-        pop     af
         ex      (sp),hl                         ; new RETurn address points at instruction after 24bit call address
-        ld      bc,restore_bank_binding         ; (HL now original caller value)
+        ld      bc,restore_bank_binding         ; (HL now restored with original caller value)
         push    bc                              ; call'ed subroutine RETurns to restore the old bank binding before
         push    de                              ; actually returning from ExtCall...
         call    RestoreBCDE
