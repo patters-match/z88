8d7e53ed9350ed5a5db42e894830c9ad4b759d79
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-11-23 21:28:06 +0000

    Optimized the commands for cleaning up the compiled files.

--- oz/cleanup.bat
+++ oz/cleanup.bat
@@ -1,25 +1,5 @@
 :: we leave *.def for other modules
 @echo off
 
-del /S /Q *.bin *.map *.obj *.bn? 2>nul >nul
-del /Q *.def
-
-cd bank0
-del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
-
-cd ..\bank1
-del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
-
-cd ..\bank2
-del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
-
-cd ..\bank3
-del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
-
-cd ..\bank6
-del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
-
-cd ..\bank7
-del /S /Q *.obj *.lst *.bin *.map *.err *.wrn 2>nul >nul
-
-cd ..
+del /S /Q *.bin *.map *.obj *.lst *.err *.wrn 2>nul >nul
+del /Q bank0\*.def bank2\*.def bank7\*.def 2>nul >nul
