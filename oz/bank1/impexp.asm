d71fea71399b6f7654beeea9d01b533b70b76ff6
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-11-30 13:06:36 +0000

    Removed space padding, using individual compiles of apps/popdowns that are combined using MakeApp with loadmap file.

--- oz/bank1/impexp.asm
+++ oz/bank1/impexp.asm
@@ -1,8 +1,35 @@
-; -----------------------------------------------------------------------------
-; Bank 1 @ S3           ROM offset $7a93
+; **************************************************************************************************
+; Imp/export popdown (Bank 1, addressed for segment 3).
+;
+; This file is part of the Z88 operating system, OZ.     0000000000000000      ZZZZZZZZZZZZZZZZZZZ
+;                                                       000000000000000000   ZZZZZZZZZZZZZZZZZZZ
+; OZ is free software; you can redistribute it and/    0000            0000              ZZZZZ
+; or modify it under the terms of the GNU General      0000            0000            ZZZZZ
+; Public License as published by the Free Software     0000            0000          ZZZZZ
+; Foundation; either version 2, or (at your option)    0000            0000        ZZZZZ
+; any later version. OZ is distributed in the hope     0000            0000      ZZZZZ
+; that it will be useful, but WITHOUT ANY WARRANTY;    0000            0000    ZZZZZ
+; without even the implied warranty of MERCHANTA-       000000000000000000   ZZZZZZZZZZZZZZZZZZZZ
+; BILITY or FITNESS FOR A PARTICULAR PURPOSE. See        0000000000000000  ZZZZZZZZZZZZZZZZZZZZ
+; the GNU General Public License for more details.
+; You should have received a copy of the GNU General Public License along with OZ; see the file
+; COPYING. If not, write to:
+;                                  Free Software Foundation, Inc.
+;                                  59 Temple Place-Suite 330,
+;                                  Boston, MA 02111-1307, USA.
+;
+; Source code was reverse engineered from OZ 4.0 (UK) ROM and made compilable by Jorma Oksanen.
+; Additional development improvements, comments, definitions and new implementations by
+; (C) Jorma Oksanen (jorma.oksanen@aini.fi), 2003
+; (C) Thierry Peycru (pek@users.sf.net), 2005
+; (C) Gunther Strube (gbs@users.sf.net), 2005
+;
+; Copyright of original (binary) implementation, V4.0:
+; (C) 1987,88 by Trinity Concepts Limited, Protechnic Computers Limited & Operating Systems Limited.
 ;
 ; $Id$
-; -----------------------------------------------------------------------------
+; ***************************************************************************************************
+
 
         Module  ImpExp
 
@@ -838,7 +865,6 @@ MSG_Eof, ERR_Esc, ERR_Name, MSG_AllSent
 .ImpExpDOR
         defp    0,0
         defp    0,0
-.nullp                                          ; !! use ImpExpDOR, not this
         defp    0,0
         defb    DM_ROM, ImpExpDORe-$PC
         defb    DT_INF, 18
@@ -850,9 +876,9 @@ MSG_Eof, ERR_Esc, ERR_Name, MSG_AllSent
         defb    0,0,0,1
         defb    AT_Good|AT_Popd,0
         defb    DT_HLP,12
-        defp    nullp,1
-        defp    nullp,1
-        defp    nullp,1
+        defp    ImpExpDOR,1
+        defp    ImpExpDOR,1
+        defp    ImpExpDOR,1
         defp    $8000,7
 
         defb    DT_NAM, ImpExpDORe-$PC-1
--- oz/bank1/pade7e7.asm
+++
@@ -1,8 +0,0 @@
-;pade7e4
-        Module  pade7e4
-
-        org     $e7e4
-
-.padding
-
-        defs    15 ($ff)                        ; !!pad spacing - to be removed when using makeapp
