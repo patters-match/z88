0c8d324515e0382f7d0145acd2c25b808817923e
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2005-12-05 21:32:35 +0000

    Tidying up the cleanup scripts for both DOS & Unix

--- oz/cleanup.sh
+++ oz/cleanup.sh
@@ -1,20 +1,32 @@
 #!/bin/bash
 
-# we leave *.def for other modules
+# **************************************************************************************************
+# OZ ROM compilation cleanup script for Unix.
+# (C) Gunther Strube (gbs@users.sf.net) 2005
+#
+# This file is part of the Z88 operating system, OZ.     0000000000000000      ZZZZZZZZZZZZZZZZZZZ
+#                                                       000000000000000000   ZZZZZZZZZZZZZZZZZZZ
+# OZ is free software; you can redistribute it and/    0000            0000              ZZZZZ
+# or modify it under the terms of the GNU General      0000            0000            ZZZZZ
+# Public License as published by the Free Software     0000            0000          ZZZZZ
+# Foundation; either version 2, or (at your option)    0000            0000        ZZZZZ
+# any later version. OZ is distributed in the hope     0000            0000      ZZZZZ
+# that it will be useful, but WITHOUT ANY WARRANTY;    0000            0000    ZZZZZ
+# without even the implied warranty of MERCHANTA-       000000000000000000   ZZZZZZZZZZZZZZZZZZZZ
+# BILITY or FITNESS FOR A PARTICULAR PURPOSE. See        0000000000000000  ZZZZZZZZZZZZZZZZZZZZ
+# the GNU General Public License for more details.
+# You should have received a copy of the GNU General Public License along with OZ; see the file
+# COPYING. If not, write to:
+#                                  Free Software Foundation, Inc.
+#                                  59 Temple Place-Suite 330,
+#                                  Boston, MA 02111-1307, USA.
+#
+# $Id: rom.bat 2019 2005-11-30 14:03:01Z gbs $
+# ***************************************************************************************************
 
-rm -f kernel.bin kernel.map kernel.obj
+# delete all compile output files in bank0 - bank7
+find . -name '*.bin *.map *.err *.obj *.lst *.err *.wrn' | xargs rm -f
 
-cd bank0
-rm -f *.obj *.bin *.map *.err
-cd ../bank1
-rm -f *.obj *.bin *.map *.err
-cd ../bank2
-rm -f *.obj *.bin *.map *.err
-cd ../bank3
-rm -f *.obj *.bin *.map *.err
-cd ../bank6
-rm -f *.obj *.bin *.map *.err
-cd ../bank7 
-rm -f *.obj *.bin *.map *.err
-
-cd ..
+# only delete *.def files for bank0 - bank7 (preserve /sysdef!)
+find . -name 'bank?/*.def' | xargs rm -f
+ 
