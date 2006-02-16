ad3cc08cdadbe6464ac1ca1c002e5929a5d3ab01
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2006-02-16 12:19:37 +0000

    Append a CR at end of file (in buffer) to avoid romupdate configuration files with the classic last missing end-of-line character.

--- z88apps/romupdate/configfile.asm
+++ z88apps/romupdate/configfile.asm
@@ -425,6 +425,8 @@
                     ex   de,hl
                     dec  hl                       ; HL points at end of block
                     ld   (bufferend),hl           ; end of buffer is byte after last newline
+                    ld   (hl),CR                  ; append a new line if last line of file is missing it...
+                    inc  hl
                     ld   (hl),0                   ; null-terminate end of loaded information
 
                     ld   de, (bufferend)          ; DE: return L-end
