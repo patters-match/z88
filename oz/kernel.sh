173a7bce5998da384ae729602418f22091ebab98
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2006-02-26 22:46:27 +0000

    Forgot to change to numerical expressions in kernel.sh script.

--- oz/kernel.sh
+++ oz/kernel.sh
@@ -30,64 +30,64 @@ COMPILE_ERROR=0
 cd bank0
 ../../tools/mpm/mpm -g ostables.asm
 cd ..
-if test "`find . -name '*.err' | wc -l`" != 0; then
+if test `find . -name '*.err' | wc -l` != 0; then
   COMPILE_ERROR=1
 fi
 
 # create lowram.def and keymap.def (address pre-compilation) for kernel0.prj and kernel7.prj compilation
 # (argument $1 contains the country localisation)
-if test "$COMPILE_ERROR" = 0; then 
+if test "$COMPILE_ERROR" -eq 0; then
   cd bank7
   ../../tools/mpm/mpm -g -I../sysdef @lowram.prj
   ../../tools/mpm/mpm -bg -DKB"$1" -I../sysdef keymap.asm
   cd ..
 fi
-if test "`find . -name '*.err' | wc -l`" != 0; then
+if test `find . -name '*.err' | wc -l` != 0; then
   COMPILE_ERROR=1
 fi
 
 # create application DOR data (binary) and address references for bank 2 compile script
-if test "$COMPILE_ERROR" = 0; then 
+if test "$COMPILE_ERROR" -eq 0; then
   cd bank7
   ../../tools/mpm/mpm -bg -I../sysdef appdors.asm
   cd ..
 fi
-if test "`find . -name '*.err' | wc -l`" != 0; then
+if test `find . -name '*.err' | wc -l` != 0; then
   COMPILE_ERROR=1
 fi
 
 # pre-compile kernel in bank 0 to resolve labels for lowram.asm
-if test "$COMPILE_ERROR" = 0; then 
+if test "$COMPILE_ERROR" -eq 0; then
   cd bank0
   ../../tools/mpm/mpm -g -I../sysdef @kernel0.prj
   cd ..
 fi
-if test "`find . -name '*.err' | wc -l`" != 0; then
+if test `find . -name '*.err' | wc -l` != 0; then
   COMPILE_ERROR=1
 fi
 
 # create final lowram binary with correct addresses from bank 0 kernel
-if test "$COMPILE_ERROR" = 0; then 
+if test "$COMPILE_ERROR" -eq 0; then
   cd bank7
   ../../tools/mpm/mpm -b -DCOMPILE_BINARY -I../sysdef @lowram.prj
   cd ..
 fi
-if test "`find . -name '*.err' | wc -l`" != 0; then
+if test `find . -name '*.err' | wc -l` != 0; then
   COMPILE_ERROR=1
 fi
 
 # compile final kernel binary for bank 7 with correct lowram code and correct bank 0 references
-if test "$COMPILE_ERROR" = 0; then 
+if test "$COMPILE_ERROR" -eq 0; then
   cd bank7
   ../../tools/mpm/mpm -bg -DCOMPILE_BINARY -DKB"$1" -I../sysdef @kernel7.prj
   cd ..
 fi
-if test "`find . -name '*.err' | wc -l`" != 0; then
+if test `find . -name '*.err' | wc -l` != 0; then
   COMPILE_ERROR=1
 fi
 
 # compile final kernel binary with OS tables for bank 0 using correct bank 7 references
-if test "$COMPILE_ERROR" = 0; then 
+if test "$COMPILE_ERROR" -eq 0; then
   cd bank0
   ../../tools/mpm/mpm -b -DCOMPILE_BINARY -I../sysdef @kernel0.prj
   ../../tools/mpm/mpm -b -DCOMPILE_BINARY ostables.asm
