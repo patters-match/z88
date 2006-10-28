9360086c44317c4056568c7c81bffb091610b257
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2006-10-28 09:02:06 +0000

    OZ ROM: Bugfix in Unix script for compilation of GN system calls.

--- oz/gn/gn.sh
+++ oz/gn/gn.sh
@@ -24,13 +24,5 @@
 # $Id$
 # ***************************************************************************************************
 
-COMPILE_ERROR=0
-
 ../../tools/mpm/mpm -b -I../def -DKB"$1" @gn.prj
-if test `find . -name '*.err' | wc -l` != 0; then
-  COMPILE_ERROR=1
-fi
 
-if test "$COMPILE_ERROR" -eq 0; then
-  ../../tools/mpm/mpm -b -I../def calc.asm
-fi
