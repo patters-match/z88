19bfdbcef29b84e16163ca90d7d09daf50fc8ebf
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2006-11-14 13:16:46 +0000

    OZ/OS_Fep: Reformatted FlashEprFileDelete source code according to OZ standard.

--- oz/os/osfep/fepfdel.asm
+++ oz/os/osfep/fepfdel.asm
@@ -23,47 +23,42 @@
 ; $Id$
 ; ***************************************************************************************************
 
-     XDEF FlashEprFileDelete
+        xdef FlashEprFileDelete
 
-     LIB FileEprFileEntryInfo
-     LIB PointerNextByte
-     LIB OZSlotPoll, SetBlinkScreen
+        lib  FileEprFileEntryInfo
+        lib  PointerNextByte
+        lib  OZSlotPoll, SetBlinkScreen
 
-     XREF FlashEprWriteByte
-     XREF SetBlinkScreenOn
+        xref FlashEprWriteByte
+        xref SetBlinkScreenOn
 
-; **************************************************************************
+; ***************************************************************************************************
 ;
 ; Standard Z88 File Eprom Format (using Flash Eprom Card).
 ;
-; Mark File Entry as deleted on File Eprom Card, identified by
-; BHL pointer, B=00h-FFh (bits 7,6 is the slot mask), HL=0000h-3FFFh is
-; the bank offset.
+; Mark File Entry as deleted on File Eprom Card, identified by BHL pointer,
+; B=00h-FFh (bits 7,6 is the slot mask), HL=0000h-3FFFh is the bank offset.
 ;
-; -------------------------------------------------------------------------
-; The screen is turned off while byte is being written when we're in the
-; same slot as the OZ ROM. During writing, no interference should happen
-; from Blink, because the Blink reads the font bitmaps each 1/100 second:
-;    When written byte is part of OZ ROM chip, the font bitmaps are suddenly
-;    unavailable which creates violent screen flickering during chip command mode.
-;    Further, and most importantly, avoid Blink doing read-cycles while
-;    chip is in command mode.
-; By switching off the screen, the Blink doesn't read the font bit maps in
-; OZ ROM, and the Flash chip can be in command mode without being disturbed
-; by the Blink.
-; -------------------------------------------------------------------------
+; --------------------------------------------------------------------------------------------------
+; The screen is turned off while byte is being written when we're in the same slot as the OZ ROM.
+; During writing, no interference should happen from Blink, because the Blink reads the font
+; bitmaps each 1/100 second:
+;    When written byte is part of OZ ROM chip, the font bitmaps are suddenly unavailable which
+;    creates violent screen flickering during chip command mode. Further, and most importantly,
+;    avoid Blink doing read-cycles while chip is in command mode.
+; By switching off the screen, the Blink doesn't read the font bit maps in OZ ROM, and the Flash
+; chip can be in command mode without being disturbed by the Blink.
+; --------------------------------------------------------------------------------------------------
 ;
 ; Important:
-; Third generation AMD Flash Memory chips may be programmed in all
-; available slots (1-3). Only INTEL I28Fxxxx series Flash chips require
-; the 12V VPP pin in slot 3 to successfully mark the File Entry as deleted
-; on the memory chip. If the Flash Eprom card is inserted in slot 1 or 2,
-; this routine will report a programming failure.
+; Third generation AMD Flash Memory chips may be programmed in all available slots (1-3). Only INTEL
+; I28Fxxxx series Flash chips require the 12V VPP pin in slot 3 to successfully mark the File Entry
+; as deleted on the memory chip. If the Flash Eprom card is inserted in slot 1 or 2, this routine
+; will report a programming failure.
 ;
-; It is the responsibility of the application (before using this call) to
-; evaluate the Flash Memory (using the FlashEprCardId routine) and warn the
-; user that an INTEL Flash Memory Card requires the Z88 slot 3 hardware, so
-; this type of unnecessary error can be avoided.
+; It is the responsibility of the application (before using this call) to evaluate the Flash Memory
+; (using the FlashEprCardId routine) and warn the user that an INTEL Flash Memory Card requires the
+; Z88 slot 3 hardware, so this type of unnecessary error can be avoided.
 ;
 ; IN:
 ;         BHL = pointer to File Entry (B=00h-FFh, HL=0000h-3FFFh bank offset)
@@ -81,43 +76,44 @@
 ;    .F....../.... different
 ;
 ; --------------------------------------------------------------------------
-; Design & Programming, Gunther Strube, Dec 1997-Apr 1998, Sept 2004
+; Design & Programming, Gunther Strube, Dec 1997-Apr 1998, Sept 2004, Nov 2006
 ; --------------------------------------------------------------------------
 ;
 .FlashEprFileDelete
-                    PUSH HL
-                    PUSH DE
-                    PUSH BC                       ; preserve CDE
-                    PUSH AF                       ; preserve AF, if possible
+        push    hl
+        push    de
+        push    bc                              ; preserve CDE
+        push    af                              ; preserve AF, if possible
 
-                    PUSH BC
-                    PUSH HL                       ; preserve File Entry pointer...
-                    CALL FileEprFileEntryInfo
-                    POP  HL
-                    POP  BC
-                    JR   C, err_delfile           ; File Entry was not found...
-                    CALL PointerNextByte          ; point at start of filename, "/"
+        push    bc
+        push    hl                              ; preserve File Entry pointer...
+        call    FileEprFileEntryInfo
+        pop     hl
+        pop     bc
+        jr      c, err_delfile                  ; File Entry was not found...
+        call    PointerNextByte                 ; point at start of filename, "/"
 
-                    LD   A,B
-                    RLCA
-                    RLCA
-                    LD   C,A                      ; BHL pointer is in slot C
-                    CALL OZSlotPoll               ; is OZ running in slot of BHL?
-                    CALL NZ,SetBlinkScreen        ; yes, blowing byte in OZ ROM (slot 0 or 1) requires LCD turned off
+        ld      a,b
+        rlca
+        rlca
+        ld      c,a                             ; BHL pointer is in slot C
+        call    OZSlotPoll                      ; is OZ running in slot of BHL?
+        call    NZ,SetBlinkScreen               ; yes, blowing byte in OZ ROM (slot 0 or 1) requires LCD turned off
 .blow_zero_byte
-                    XOR  A
-                    LD   C,A                      ; indicate file deleted (0)
-                    LD   E,A                      ; E = 0, blow byte to either INTEL or AMD chip
-                    CALL FlashEprWriteByte        ; mark file as deleted with 0 byte
-                    JR   C, err_delfile
+        xor     a
+        ld      c,a                             ; indicate file deleted (0)
+        ld      e,a                             ; E = 0, blow byte to either INTEL or AMD chip
+        call    FlashEprWriteByte               ; mark file as deleted with 0 byte
+        jr      c, err_delfile
 
-                    POP  AF
-                    CP   A                        ; Fc = 0, Fz = 1
+        pop     af
+        cp      a                               ; Fc = 0, Fz = 1
 .exit_delfile
-                    CALL SetBlinkScreenOn         ; always turn on screen after FlashEprWriteByte
-                    POP  BC                       ; (turning screen on, with screen already on has no effect...)
-                    POP  DE
-                    POP  HL
-                    RET
-.err_delfile        POP  BC                       ; remove old AF, use new AF (error code and Fc = 1)
-                    JR   exit_delfile
+        call    SetBlinkScreenOn                ; always turn on screen after FlashEprWriteByte
+        pop     bc                              ; (turning screen on, with screen already on has no effect...)
+        pop     de
+        pop     hl
+        ret
+.err_delfile
+        pop     bc                              ; remove old AF, use new AF (error code and Fc = 1)
+        jr      exit_delfile
