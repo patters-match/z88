71cd78f4aa0688b642d3668f7d8926a35161f711
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2007-01-28 18:59:28 +0000

    /z88apps/flashstore: Final text adjustments to File Browse Help Page.

--- z88apps/flashstore/browse.asm
+++ z88apps/flashstore/browse.asm
@@ -912,13 +912,13 @@ Module BrowseFiles
 ; constants
 ;
 .browsehelp         defm 1, "2JC", 1,"3-SC"
-                    defm 1, "TFILE BROWSING KEYS", 1, "T", 13, 10, 13, 10
-                    defm     "Single file   ", 1, SD_ODWN, 1, SD_OUP, 13, 10
-                    defm     "Page", 1, "3N", 32+7, ' ', 1, '-', 1, SD_ODWN, 1, SD_OUP, 13, 10
-                    defm     "First, Last  ", 1, '+', 1, SD_ODWN, 1, SD_OUP, 13, 10
+                    defb 13, 10
+                    defm     "Previous,Next ", 1, SD_OUP, 1, SD_ODWN, 13, 10
+                    defm     "Page", 1, "3N", 32+7, ' ', 1, '-', 1, SD_OUP, 1, SD_ODWN, 13, 10
+                    defm     "First,Last   ", 1, '+', 1, SD_OUP, 1, SD_ODWN, 13, 10, 13, 10
                     defm     "Fetch to RAM  ", 1, SD_ENT, 1, "BF", 1, "B",  13, 10
-                    defm     "Mark deleted  ", 1, MU_DEL, 1, "BD", 1, "B",  13, 10
-                    defm     "Copy to slot X   ", 1, "BC", 1, "B",  13, 10
+                    defm     "Mark Deleted  ", 1, MU_DEL, 1, "BD", 1, "B",  13, 10
+                    defm     "Copy to Slot     ", 1, "BC", 1, "B",  13, 10
                     defb 0
 
 .filearea_banner    defm "FILE AREA [VIEW/FETCH/DELETE "
