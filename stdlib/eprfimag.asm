190d43e494dce502deab8fe95796fbf54e9f6c4c
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2007-03-06 18:35:27 +0000

    /stdlib/FileEprFileImage: Optimized code & shaved off a few bytes of total size of routine.

--- stdlib/eprfimag.asm
+++ stdlib/eprfimag.asm
@@ -3,7 +3,7 @@
 ; **************************************************************************************************
 ; This file is part of the Z88 Standard Library.
 ;
-; The Z88 Standard Library is free software; you can redistribute it and/or modify it under 
+; The Z88 Standard Library is free software; you can redistribute it and/or modify it under
 ; the terms of the GNU General Public License as published by the Free Software Foundation;
 ; either version 2, or (at your option) any later version.
 ; The Z88 Standard Library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
@@ -12,11 +12,11 @@
 ; You should have received a copy of the GNU General Public License along with the
 ; Z88 Standard Library; see the file COPYING. If not, write to the
 ; Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-; 
-; $Id$  
+;
+; $Id$
 ;
 ;***************************************************************************************************
-     
+
      LIB FileEprRequest
      LIB FileEprFileEntryInfo
      LIB AddPointerDistance
@@ -31,10 +31,10 @@
 ; area in application cards (below application banks in first free 64K boundary)
 ;
 ; Return pointer to start of file image of File Entry at BHL
-; (B=00h-FFh embedded slot mask, HL=0000h-3FFFh bank offset) 
+; (B=00h-FFh embedded slot mask, HL=0000h-3FFFh bank offset)
 ;
 ; IN:
-;    BHL = pointer to Eprom File Entry in card at slot 
+;    BHL = pointer to Eprom File Entry in card at slot
 ;
 ; OUT:
 ;    Fc = 0, File Eprom available, File Entry available
@@ -45,15 +45,14 @@
 ;         File Eprom was not found in slot, or File Entry not available
 ;
 ; Registers changed after return:
-;    A..CDE../IXIY same
-;    .FB...HL/.... different
+;    ...CDE../IXIY same
+;    AFB...HL/.... different
 ;
 ; ------------------------------------------------------------------------
 ; Design & programming by Gunther Strube, InterLogic, Dec 1997 - Aug 1998
 ; ------------------------------------------------------------------------
 ;
 .FileEprFileImage   PUSH DE
-                    PUSH AF
                     PUSH BC
 
                     PUSH BC
@@ -61,7 +60,7 @@
                     CALL FileEprFileEntryInfo
                     POP  HL
                     POP  BC
-                    JR   C, no_entry              ; No files are present on File Eprom...
+                    JR   C, exit_FileEprFileImage ; No files are present on File Eprom...
 
                     INC  A                        ; length of filename + length byte
                     ADD  A,4                      ; + 4 bytes (32bit integer containing file size)
@@ -70,18 +69,9 @@
                     LD   D,C
                     LD   E,A
                     CALL AddPointerDistance       ; BHL = start of file image...
-
+.exit_FileEprFileImage
                     POP  DE
                     LD   C,E                      ; original C register restored
-                    POP  DE
-                    LD   A,D                      ; original A restored...
                     POP  DE                       ; original DE restored
                     RET
 
-.no_entry           POP  DE
-                    LD   C,E                      ; original C register restored
-                    POP  DE                       ; old AF...
-                    SCF
-                    LD   A, RC_Onf                ; return error code "Object not found"
-                    POP  DE
-                    RET
