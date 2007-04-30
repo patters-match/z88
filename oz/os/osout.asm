b8e1b0a4411a70a2c14e0099b7572ac8be8bcfbd
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2007-04-30 12:58:44 +0000

    OZ, OS_Out, micro-optimisation: save a single memory cycle and a few T-states.

--- oz/os/osout.asm
+++ oz/os/osout.asm
@@ -24,15 +24,17 @@ xref    OSOutMain                               ; bank7/scrdrv1.asm
 .OSOut
         call    OSFramePush
 
-        ld      bc, (ubCLIActiveCnt)
-        inc     c
-        dec     c
+        ld      c,a
+        ld      a, (ubCLIActiveCnt)
+        or      a
+        ld      a,c
         jr      z, osout_1
         OZ      DC_Out                          ; Write to CLI
         jr      osout_2
-
 .osout_1
         call    OSOutMain
-
 .osout_2
         jp      osfpop_1
+
+
+
