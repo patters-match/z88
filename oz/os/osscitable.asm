fa17659e89cf2579c3da6cabbdb05a65b93148d6
pek <pek@870f116b-6601-0410-b2b2-9324e3df3746>
2007-07-12 20:40:22 +0000

    Verify page crossing in OS_Sci init table. This file should be merged with OSSci.asm

--- oz/os/osscitable.asm
+++ oz/os/osscitable.asm
@@ -6,5 +6,10 @@ xdef    OSSciTable
 ; must be in the same page
 
 .OSSciTable
+.OSSciTbl
         defb 0, 3, 6, 7, 5, 5                   ; #low bits ignored
+.OSSciTbl_end
 
+IF ($linkaddr(OSSciTbl) / 256) <> ($linkaddr(OSSciTbl_end) / 256)
+        ERROR "OS_SCI table crosses address page boundary!"
+ENDIF
