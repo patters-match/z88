c1160aea234852e2ebe8577668f79ba14ddbb291
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2008-03-31 09:38:08 +0000

    /OZ, knlbind.asm: Added SVN properties "eol-style: native" and "svn-keywords" (the usual two) for sources to have operating system native line feeds (for checkouts) and keyword expansion for ID keyword (to show lates changes statistics line).

--- oz/os/knlbind.asm
+++ oz/os/knlbind.asm
@@ -27,7 +27,7 @@
 ; Copyright of original (binary) implementation, V4.0:
 ; (C) 1987,88 by Trinity Concepts Limited, Protechnic Computers Limited & Operating Systems Limited.
 ;
-; $Id: knlbind.asm $
+; $Id$
 ; ***************************************************************************************************
 
         module  knlbind
