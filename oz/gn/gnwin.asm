413533f912151860319da1eadb37dc4cbdd45a90
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2008-07-09 09:03:50 +0000

    OZ/GN_Win, OZ-7: New API; bit 4 will grey screen, before window is drawn. More code optimisations.

--- oz/gn/gnwin.asm
+++ oz/gn/gnwin.asm
@@ -40,20 +40,21 @@
 ;       BHL = pointer to 7 byte window definition block (if B=0, then local pointer)
 ;
 ;           Window Defintion Block offsets:
-;           0:  A = window id. The ID is in range 1-6 (7 is used by OZ).
-;               bit 7=1, 6=1, 5=1: Extended window: left & right shelf brackets, left & right bars, 8 pixel inverted top banner and bottom line
-;               bit 7=1, 6=1, 5=0: Extended window: left & right shelf brackets, left & right bars, 7 pixel inverted top banner and bottom line
-;               bit 7=1, 6=0, 5=1: Standard window: left & right shelf brackets, left & right bars and 8 pixel inverted banner
-;               bit 7=1, 6=0, 5=0: Standard window: left & right shelf brackets, left & right bars and 7 pixel inverted banner
-;               bit 7=0, 6=1: Standard window: left & right bars, no shelf brackets, no banner, no bottom line.
-;               bit 7=0, 6=0: Standard window: no bars, no shelf brackets, no banners.
+;           0:  A = window ID (bits 0-3). The ID is in range 1-8
+;               bit 7=1, 6=1: Draw left & right bars, shelf brackets, banner text, bottom line
+;               bit 7=1, 6=0: Draw left & right bars, shelf brackets, banner text
+;               bit 7=0, 6=1: Draw left & right bars
+;               bit 7=0, 6=0: Just create window space (no visible borders)
+;               bit 5=1: 8 pixel inverted top banner (only evaluated with bit 7=1)
+;               bit 5=0: 7 pixel inverted top banner (only evaluated with bit 7=1)
+;               bit 4=1: For all windows: grey screen, then draw window to get a pop-up effect.
 ;           1:  X coordinate (upper left corner) of Window
 ;           2:  Y coordinate (upper left corner) of Window
 ;           3:  WIDTH of Window (inclusive banner & bottom line)
 ;           4:  HEIGHT of Window (inclusive banner & bottom line)
 ;           5:  low byte, high byte address of window banner text
 ;               Only specified if bit 7 of window ID is enabled.
-;               Set pointer to 0, if using a dynamic banner (to create the window with different banner each time)
+;               Set pointer to 0, if using a dynamic banner (window with different banner each time)
 ;
 ;       Example (Extended window "2" with 8 pixel banner and bottom line):
 ;           defb @11100000 | 2
@@ -65,8 +66,8 @@
 ;
 ; Register status on return:
 ;
-; AFBCDEHL/IXIY  same
-; ......../....  different
+; A.BCDEHL/IXIY  same
+; .F....../....  different
 ;
 ; ----------------------------------------------------------------------
 ; Design & programming by Gunther Strube, 1995-2008
@@ -85,28 +86,30 @@
         ld      e,(ix+3)                        ; W
         ld      d,(ix+4)                        ; H
 
+        oz      OS_Pout                         ; init base window "1"
+        defm    1,"7#1",32,32,32+94,32+8,128,1,"2H1",0
+
+        bit     4,a
+        jr      z, check_wintype
+        oz      OS_Pout
+        defm    1,"2G+",0                       ; grey entire screen (but not left side MTH menu window)
+
+.check_wintype
         bit     7,a
-        jr      nz, extended_window             ; create extended window (banner, bottom line)
+        jr      nz, extended_window             ; create extended window (bars, banner, bottom line)
         bit     6,a
         jr      nz, stdwindow_bars
 .stdwindow_nobars
-        call    AscWindowID                     ; mask out type bits and convert to Ascii Window ID
-        push    af
         call    stdwindow_dimens
-        ld      a,128                           ; create standard window without bars
+        ld      a,128                           ; create standard window without bars (bit 7=0,6=0)
         jr      resetw
 
 .stdwindow_bars
-        call    AscWindowID                     ; mask out type bits and convert to Ascii Window ID
-        push    af
         call    stdwindow_dimens
         ld      a,129                           ; create standard window with bars
 .resetw
         oz      Os_Out
-        oz      OS_Pout
-        defm    1,"2C",0                        ; clear window and enable cursor.
-        pop     af
-        oz      OS_Out                          ; window ID to reset.
+        call    clswin                          ; clear window and enable cursor.
 .exit_gnwin
         pop     ix
         pop     de
@@ -115,32 +118,18 @@
 
 .extended_window
 ; window with banner, right & left bars, optional bottom line...
-
-        call    AscWindowID                     ; mask out type bits and convert to Ascii Window ID
-        push    af                              ; window id on stack
-
-        oz      OS_Pout                         ; init base window "1"
-        defm    1,"7#1",32,32,32+94,32+8,128,1,"2H1"
-        defm    1,"4-SCR",0
+        bit     6,a
+        jr      z, not_draw_botline
 
         oz      OS_Pout
         defm    1,"3@",0                        ; VDU cursor position (x and y sent below..)
-
         ld      a,c
         add     a,32
         oz      Os_Out                          ; X position stored...
         ld      a,b
         add     a,d
-        add     a,32                            ; y+height
-        bit     6,(ix+0)
-        jr      z, no_adj_botline0
-        sub     1                               ; y+height-1 (for bottom line)
-.no_adj_botline0
+        add     a,31                            ; y+height-1 (for bottom line)
         oz      Os_Out
-
-        bit     6,(ix+0)
-        jr      z, not_draw_botline             ; the bottom line was not defined in the window attribute byte.
-
         oz      OS_Pout
         defm    1,"2*",'I',0                    ; first display bottom left corner
         ld      a,e
@@ -155,8 +144,7 @@
 .not_draw_botline
         oz      OS_Pout
         defm    1,"7#",0                        ; now create window
-        pop     af
-        push    af
+        call    AscWindowID
         oz      Os_Out                          ; window ID
         inc     c                               ; window adjusted to bottom line
         ld      a,c
@@ -179,10 +167,7 @@
         oz      Os_Out
         ld      a, @10000011                    ; bars, shelf brackets ON
         oz      OS_out
-        oz      OS_Pout
-        defm    1,"2C",0                        ; select & clear window area
-        pop     af
-        oz      Os_Out                          ; of id in A
+        call    clswin
 
         bit     5,(ix+0)
         jr      z, banner7pxl
@@ -197,7 +182,6 @@
         defm    1,"2JC"                         ; centre text
         defm    1,"3@",$20,$20,0                ; set cursor at (0,0) in window
 
-        push    af                              ; window id back on stack
         call    getBanner                       ; get banner pointer in HL
         oz      Gn_Sop                          ; write banner at top line of window
 
@@ -209,7 +193,7 @@
 
         oz      OS_Pout
         defm    1,"7#",0                        ; now re-create window within window
-        pop     af
+        call    AscWindowID
         oz      Os_Out                          ; with id in A
         ld      a,c
         add     a,32
@@ -240,6 +224,7 @@
 .stdwindow_dimens
         oz      OS_Pout
         defm    1,"7#",0
+        call    AscWindowID                     ; mask out type bits and convert to Ascii Window ID
         oz      Os_Out                          ; VDU 1,"7#",<ID>
         ld      a,c
         add     a,32
@@ -284,6 +269,17 @@
 ; ******************************************************************************
 ; convert integer to
 .AscWindowID
+        ld      a,(ix+0)
         and     @00001111                       ; mask out window type bits...
         or      @00110000                       ; adjust for Ascii "0" - "9"
+        ret
+
+; ******************************************************************************
+; clear window (of specified ID in window definition block)
+;
+.clswin
+        oz      OS_Pout
+        defm    1,"2C",0                        ; clear window and enable cursor.
+        call    AscWindowID                     ; mask out type bits and convert to Ascii Window ID
+        oz      OS_Out                          ; window ID to reset.
         ret
