b0ba4713a6e7a15d7830b7c77aa0341993106d60
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2008-09-10 19:52:06 +0000

    /OZ/GN_*: Added GPL header to source files.

--- oz/gn/gnfile.asm
+++ oz/gn/gnfile.asm
@@ -1,8 +1,34 @@
-; -----------------------------------------------------------------------------
-; Bank 3 @ S3
+; **************************************************************************************************
+; High level file API for file open, close, delete and rename (GN_Opf, GN_Cl, GN_Del, GN_Ren).
+;
+; This file is part of the Z88 operating system, OZ.     0000000000000000      ZZZZZZZZZZZZZZZZZZZ
+;                                                       000000000000000000   ZZZZZZZZZZZZZZZZZZZ
+; OZ is free software; you can redistribute it and/    0000            0000              ZZZZZ
+; or modify it under the terms of the GNU General      0000            0000            ZZZZZ
+; Public License as published by the Free Software     0000            0000          ZZZZZ
+; Foundation; either version 2, or (at your option)    0000            0000        ZZZZZ
+; any later version. OZ is distributed in the hope     0000            0000      ZZZZZ
+; that it will be useful, but WITHOUT ANY WARRANTY;    0000            0000    ZZZZZ
+; without even the implied warranty of MERCHANTA-       000000000000000000   ZZZZZZZZZZZZZZZZZZZZ
+; BILITY or FITNESS FOR A PARTICULAR PURPOSE. See        0000000000000000  ZZZZZZZZZZZZZZZZZZZZ
+; the GNU General Public License for more details.
+; You should have received a copy of the GNU General Public License along with OZ; see the file
+; COPYING. If not, write to:
+;                                  Free Software Foundation, Inc.
+;                                  59 Temple Place-Suite 330,
+;                                  Boston, MA 02111-1307, USA.
+;
+; Source code was reverse engineered from OZ 4.0 (UK) ROM and made compilable by Jorma Oksanen.
+; Additional development improvements, comments, definitions and new implementations by
+; (C) Jorma Oksanen (jorma.oksanen@gmail.com), 2003
+; (C) Thierry Peycru (pek@users.sf.net), 2005,2006
+; (C) Gunther Strube (gbs@users.sf.net), 2005,2006
+;
+; Copyright of original (binary) implementation, V4.0:
+; (C) 1987,88 by Trinity Concepts Limited, Protechnic Computers Limited & Operating Systems Limited.
 ;
 ; $Id$
-; -----------------------------------------------------------------------------
+; ***************************************************************************************************
 
         Module GNFile
 
@@ -22,13 +48,13 @@ xdef    GNRen
 
 ;       ----
 
-xref    CompressFN              
-xref    FreeDOR                 
-xref    GetFsNodeDOR            
-xref    GetOsf_BHL              
-xref    GN_ret1a                
-xref    IsSegSeparator          
-xref    PutOsf_Err              
+xref    CompressFN
+xref    FreeDOR
+xref    GetFsNodeDOR
+xref    GetOsf_BHL
+xref    GN_ret1a
+xref    IsSegSeparator
+xref    PutOsf_Err
 
 ;       ----
 
@@ -66,7 +92,7 @@ xref    PutOsf_Err
         cp      OP_DIR
         jp      z, opf_errIvf
         cp      OP_OUT
-        jp      z, opf_errIvf           
+        jp      z, opf_errIvf
 
 ;       find first file matching wildcard string
 
--- oz/gn/gnfname1.asm
+++ oz/gn/gnfname1.asm
@@ -1,8 +1,34 @@
-; -----------------------------------------------------------------------------
-; Bank 3 @ S3
+; **************************************************************************************************
+; GN_Fcm, GN_Fex and miscellaneous internal filename functionality
+;
+; This file is part of the Z88 operating system, OZ.     0000000000000000      ZZZZZZZZZZZZZZZZZZZ
+;                                                       000000000000000000   ZZZZZZZZZZZZZZZZZZZ
+; OZ is free software; you can redistribute it and/    0000            0000              ZZZZZ
+; or modify it under the terms of the GNU General      0000            0000            ZZZZZ
+; Public License as published by the Free Software     0000            0000          ZZZZZ
+; Foundation; either version 2, or (at your option)    0000            0000        ZZZZZ
+; any later version. OZ is distributed in the hope     0000            0000      ZZZZZ
+; that it will be useful, but WITHOUT ANY WARRANTY;    0000            0000    ZZZZZ
+; without even the implied warranty of MERCHANTA-       000000000000000000   ZZZZZZZZZZZZZZZZZZZZ
+; BILITY or FITNESS FOR A PARTICULAR PURPOSE. See        0000000000000000  ZZZZZZZZZZZZZZZZZZZZ
+; the GNU General Public License for more details.
+; You should have received a copy of the GNU General Public License along with OZ; see the file
+; COPYING. If not, write to:
+;                                  Free Software Foundation, Inc.
+;                                  59 Temple Place-Suite 330,
+;                                  Boston, MA 02111-1307, USA.
+;
+; Source code was reverse engineered from OZ 4.0 (UK) ROM and made compilable by Jorma Oksanen.
+; Additional development improvements, comments, definitions and new implementations by
+; (C) Jorma Oksanen (jorma.oksanen@gmail.com), 2003
+; (C) Thierry Peycru (pek@users.sf.net), 2005,2006
+; (C) Gunther Strube (gbs@users.sf.net), 2005,2006
+;
+; Copyright of original (binary) implementation, V4.0:
+; (C) 1987,88 by Trinity Concepts Limited, Protechnic Computers Limited & Operating Systems Limited.
 ;
 ; $Id$
-; -----------------------------------------------------------------------------
+; ***************************************************************************************************
 
         Module GNFName1
 
