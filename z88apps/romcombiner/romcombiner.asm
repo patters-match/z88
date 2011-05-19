d793c22ba693b6153556b84f5565dc86c0377802
gbs <gbs@870f116b-6601-0410-b2b2-9324e3df3746>
2011-05-19 17:50:40 +0000

    /z88apps/RomCombiner, V2.08 (work sponsored by Rakewell): Supports writing to flash cards in all slots (not jsut hard coded to slot 3).

--- z88apps/romcombiner/romcombiner.asm
+++ z88apps/romcombiner/romcombiner.asm
@@ -1,6 +1,6 @@
 ; *************************************************************************************
 ; RomCombiner
-; (c) Garry Lancaster, 2000-2006 (yahoogroups@zxplus3e.plus.com)
+; (c) Garry Lancaster, 2000-2011 (yahoogroups@zxplus3e.plus.com)
 ;
 ; RomCombiner is free software; you can redistribute it and/or modify it under the terms of the
 ; GNU General Public License as published by the Free Software Foundation;
@@ -25,7 +25,7 @@
      
      org $b500
 
-     ; These libraries refer to V0.8 of stdlib:
+     ; These libraries refer to V0.8 or greater of stdlib:
      lib FlashEprCardID, FlashEprCardErase, FlashEprBlockErase, FlashEprWriteBlock
 
      include "flashepr.def"
@@ -40,16 +40,15 @@
      jp   eraseflash    ; $B909
      jp   crc32bank     ; $B90C
 
-; The BLOWBANK routine blows page B (00-3f) to standard EPROM type C,
+; The BLOWBANK routine blows bank B (00-ff) to standard EPROM type C,
 ; with data stored at DE. On exit, HL=0 if successful, or address
 ; of failed byte (in segment 3)
-; If C=0, blows a page of a Flash EPROM
+; If C=0, blows a bank of a Flash EPROM
 
 .blowbank
      xor  a
      cp   c              ; is it a FLASH eprom?
      jr   z,blowflash    ; move on if so
-     di
      ld   hl,$c000
      ld   a,($4D3)
      push af
@@ -96,23 +95,32 @@
      pop  af
      ld   ($4D3),a
      out  ($D3),a
-     ei
      ret
 
-.blowflash
-     push bc
-     ld   c,3            ; use slot 3 as default for all Flash Cards
+.blowflash               ; DE points at buffer to blow
+     push bc             ; bank B contains slot specifier
+     ld   a,b
+     and  @11000000
+     rlca
+     rlca
+     ld   c,a            ; slot C (of bank B)
      call FlashEprCardID
      pop  bc
      ld   hl,$c000       ; destination start address of bank (use segment 3 to blow data)
      ret  c              ; exit if not Flash card
-     set  7,b            ; A = Flash Card type (returned from FlashEprCardID)
-     set  6,b            ; bank no. in B points into slot 3
-     ld   iy,$4000       ; whole bank
+     ld   iy,256         ; blow bank in 256 byte blocks
+     ld   c,a            ; copy of Flash type
+.flash_blow_bank_loop
+     ld   a,c            ; A = flash type programming
      call FlashEprWriteBlock
      ret  c
-     ld   hl,0           ; indicate success to BBC BASIC
-     ret
+     inc  d              ; get ready to blow next page of 16K buffer
+     ld   a,h
+     and  $3f            ; remove segment specifier
+     or   l              ; when HL = 0, whole bank has been blown...
+     jr   nz,flash_blow_bank_loop     
+     ld   hl,0000        ; HL = 0 indicate success to BBC BASIC
+     ret                 
 
 ; Subroutine to check blocks of an EPROM are properly erased
 ; On entry, B=bank to check (00-3f)
@@ -155,11 +163,9 @@
      jr   exit2
 
 ; Subroutine to erase blocks of a Flash EPROM
-; On entry, E=block to erase ($ff=all)
+; On entry, C=slot number, E=block to erase ($ff=all)
 ; On exit, HL=0 if no error, or $ffff if error
-
 .eraseflash
-     ld   c,3
      call FlashEprCardID ; check for Flash EPROM in slot 3
      ld   hl,$ffff
      jr   c,eraseerr     ; exit if not Flash device
