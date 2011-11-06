3e776f4c161b10feceb1d2514835a0b6c2bab85a
Garry Lancaster <garry@garrylancaster.me.uk>
2011-11-06 19:34:47 +0000

    /z88apps/installer: Added BASIC Installer
    Added build and support files for the "BASIC Installer".

---
+++ z88apps/installer/makebasinstaller.bat
@@ -0,0 +1,25 @@
+:: *************************************************************************************
+::
+:: Installer/Bootstrap/Packages compile script for DOS/Windows
+:: Installer/Bootstrap/Packages (c) Garry Lancaster 1998-2011
+::
+:: Installer/Bootstrap/Packages is free software; you can redistribute it and/or modify
+:: it under the terms of the GNU General Public License as published by the Free Software
+:: Foundation; either version 2, or (at your option) any later version.
+:: Installer/Bootstrap/Packages is distributed in the hope that it will be useful, but
+:: WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+:: FOR A PARTICULAR PURPOSE.
+:: See the GNU General Public License for more details.
+:: You should have received a copy of the GNU General Public License along with
+:: Installer/Bootstrap/Packages; see the file COPYING. If not, write to the
+:: Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+::
+:: $Id$
+::
+:: *************************************************************************************
+
+del *.obj *.sym *.bin *.map installer.c
+
+:: Assemble the BASIC installer codefile
+..\..\tools\mpm\mpm -b -oinstaller.c -I..\..\oz\def -DBASINST @basinstaller.prj
+
