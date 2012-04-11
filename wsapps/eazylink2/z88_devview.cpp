c1a9df7860e134bd0208f90837a1cdf0061152a7
Oscar <oernohaz@SSNMacBookPro.local>
2012-04-10 23:16:54 -0700

    Create a Z88 File Viewer.     A tree view similar to the native OS file Browser     Jira EAZ-4

---
+++ wsapps/eazylink2/z88_devview.cpp
@@ -0,0 +1,349 @@
+/*********************************************************************************************
+
+ EazyLink2 - Fast Client/Server Z88 File Management
+ (C) Gunther Strube (gstrube@gmail.com) 2011
+  & Oscar Ernohazy 2012
+
+ EazyLink2 is free software; you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation;
+ either version 2, or (at your option) any later version.
+ EazyLink2 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License along with EazyLink2;
+ see the file COPYING. If not, write to the
+ Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+**********************************************************************************************/
+
+#include <QListIterator>
+#include "z88_devview.h"
+
+/**
+  * the Z88 File Selection class constructor.
+  * @param fspec is the file name.
+  * @param type is the item type, dir or file.
+  */
+Z88_Selection::Z88_Selection(QString fspec, Z88_DevView::entryType type)
+    :m_fspec(fspec),
+    m_type(type)
+{
+
+}
+
+/**
+  * The Z88 Device View Class Constructor.
+  * @param devname is the name of the Z88 Storage Device. ie :RAM.1
+  * @param com_thread is the communications thread to use for I/O.
+  * @param parent is the owner QWidget.
+  */
+Z88_DevView::Z88_DevView(const QString devname, CommThread &com_thread, QWidget *parent) :
+    QTreeWidget(parent),
+    m_cthread(com_thread),
+    m_devname(devname),
+    m_selChangeLock(false)
+{
+    setObjectName("Z88Tree");
+
+    setColumnCount(4);
+
+    QStringList hdrList;
+    hdrList << "File or Directory" << "Size" << "Creation Date & Time" << "Modified Date & Time";
+
+    setHeaderLabels(hdrList);
+    setSortingEnabled (true);
+    sortItems(0, Qt::AscendingOrder);
+    setColumnWidth(0,200);
+    setColumnWidth(1,68);
+    setColumnWidth(2,163);
+    setColumnWidth(3,163);
+
+
+    setSelectionMode(QAbstractItemView::ExtendedSelection);
+}
+
+/**
+  * Get the Z88 Device name, ie :RAM.1
+  */
+const QString &Z88_DevView::getDevname()
+{
+    return m_devname;
+}
+
+/**
+  * Insert a Unique Z88 Filename or Directory names into the Tree view.
+  * @param fspec_list is split list of a single file or dir, broken into parts.
+  *  example a file :RAM.1/dir1/foo.txt would be 3 entries in the list ":RAM.1" "dir1" "foo.txt"
+  * @param d_type is the type of entry, dir or file.
+  * @param fsize is the Size of the file, or blank if not enabled.
+  * @param fcreate_date is the File Creation date, or leave blank if not enabled.
+  * @param fmod_date is the File Modified date, or leave blank if not enabled.
+  * @return true on success.
+  */
+bool Z88_DevView::insertUniqueFspec(QStringList &fspec_list, entryType d_type,
+                                    const QString &fsize, const QString &fcreate_date,
+                                    const QString &fmod_date)
+{
+    QStringList entry;
+
+    if(!fspec_list.isEmpty()){
+        /**
+         * Sanity Check to make sure the files belong in this device display
+         */
+        if(fspec_list[0] != m_devname){
+            return false;
+        }
+
+        /**
+          * Strip the Device name
+          */
+        fspec_list.removeFirst();
+
+        if(!fspec_list.isEmpty()){
+            QList<QTreeWidgetItem *> treeItem;
+            treeItem = findItems(fspec_list[0], Qt::MatchExactly, 0);
+
+            /**
+              * If the Dir or file is not already in the tree
+              */
+            if(treeItem.isEmpty()){
+                entry << fspec_list[0];
+
+               /**
+                 * Only Append Time and date of a file, not a dir
+                 */
+                if(fspec_list.count() == 1){
+                    if(!fsize.isEmpty()){
+                        entry << fsize;
+                    }
+                    else{
+                        entry << " ";
+                    }
+
+                    if(!fcreate_date.isEmpty()){
+                        entry << fcreate_date;
+                    }
+                    else{
+                        entry << " ";
+                    }
+
+                    if(!fmod_date.isEmpty()){
+                        entry << fmod_date;
+                    }
+                    else{
+                        entry << " ";
+                    }
+                }
+                QTreeWidgetItem *qt = new QTreeWidgetItem(entry, d_type);
+                addTopLevelItem(qt);
+
+                fspec_list.removeFirst();
+
+                if(fspec_list.isEmpty()){
+                    return true;
+                }
+                return insertFspecList(qt, fspec_list, d_type, fsize, fcreate_date, fmod_date);
+            }
+
+            /**
+              * Parent dir exists
+              */
+            fspec_list.removeFirst();
+
+            if(!fspec_list.isEmpty()){
+                return insertFspecList(treeItem[0], fspec_list, d_type, fsize, fcreate_date, fmod_date);
+            }
+        }
+    }
+    return false;
+}
+
+/**
+  * Insert a Z88 Filename or Directory names into the Tree view.
+  * @param parent is the QTreeWidgetItem to inser the Child after.
+  * @param fspec_list is split list of a single file or dir, broken into parts.
+  *  example a file :RAM.1/dir1/foo.txt would be 3 entries in the list ":RAM.1" "dir1" "foo.txt"
+  * @param d_type is the type of entry, dir or file.
+  * @param fsize is the Size of the file, or blank if not enabled.
+  * @param fcreate_date is the File Creation date, or leave blank if not enabled.
+  * @param fmod_date is the File Modified date, or leave blank if not enabled.
+  * @return true on success.
+  */
+bool Z88_DevView::insertFspecList(QTreeWidgetItem *parent, QStringList &fspec_list, entryType d_type,
+                                  const QString &fsize, const QString &fcreate_date, const QString &fmod_date )
+{
+    QStringList entry;
+    QTreeWidgetItem *qt;
+
+    int count;
+    if(!fspec_list.isEmpty()){
+
+        count = parent->childCount();
+        /**
+          * Search all the Children Here for Duplicates
+          */
+        for(int idx = 0; idx < count; idx++){
+            if(parent->child(idx)->text(0) == fspec_list[0]){
+                fspec_list.removeFirst();
+
+                if(fspec_list.isEmpty()){
+                    return false;
+                }
+                return insertFspecList(parent->child(idx), fspec_list, d_type, fsize, fcreate_date, fmod_date);
+            }
+        }
+
+        entry << fspec_list[0];
+
+        /**
+          * Only insert size and date for files.
+          */
+        if(fspec_list.count() == 1){
+            if(!fsize.isEmpty()){
+                entry << fsize;
+            }
+            else{
+                entry << " ";
+            }
+
+            if(!fcreate_date.isEmpty()){
+                entry << fcreate_date;
+            }
+            else{
+                entry << " ";
+            }
+
+            if(!fmod_date.isEmpty()){
+                entry << fmod_date;
+            }
+            else{
+                entry << " ";
+            }
+        }
+
+        qt = new QTreeWidgetItem(entry, d_type);
+
+        parent->addChild(qt);
+
+        fspec_list.removeFirst();
+
+        if(fspec_list.isEmpty()){
+            return true;
+        }
+        return insertFspecList(qt, fspec_list, d_type, fsize, fcreate_date, fmod_date);
+    }
+    return false;
+}
+
+/**
+  * Get the Fully qualified file name for the specified Item
+  * @param item is the QTreeWidgetItem
+  * @paramfspec is filled in file spec class.
+  * @return the File Spec
+  */
+const Z88_Selection &Z88_DevView::getItemFspec(QTreeWidgetItem *item, Z88_Selection &fspec)const
+{
+    QString fname;
+    fspec.m_fspec = m_devname + "/";
+
+    if(item){
+        fspec.m_type = (item->type() == type_File) ? type_File : type_Dir;
+    }
+    else{
+        fspec.m_type = Z88_DevView::type_Dir;
+    }
+
+    /**
+      * Build the Full filename by backing up to the top of the tree
+      */
+    while(item){
+        fname.prepend(item->text(0));
+        item = item->parent();
+        if(!item)break;
+        fname.prepend("/");
+    }
+    fspec.m_fspec += fname;
+
+    return fspec;
+}
+
+/**
+  * Get a list of Sub Items below the specified item entry.
+  * @param item is the top level selection item.
+  * @param selections is the list to be filled in.
+  * @return the list of selections.
+  */
+const QList<Z88_Selection> &Z88_DevView::getItemChildren(QTreeWidgetItem *item, QList<Z88_Selection> &selections) const
+{
+    QTreeWidgetItem *child;
+    int childcount = item->childCount();
+
+    for(int x=0; x < childcount; x++){
+        child = item->child(x);
+
+        Z88_Selection z88Selection;
+
+        getItemFspec(child, z88Selection);
+        selections.append(z88Selection);
+
+        if(child->childCount()){
+            getItemChildren(child, selections);
+        }
+    }
+    return selections;
+}
+
+/**
+  * Get the List of selected Files and Directories.
+  * @param recurse set to true to get all the sub files within selected directories.
+  * @return the list of fully qualified file names and directories.
+  */
+QList<Z88_Selection> *Z88_DevView::getSelection(bool recurse)
+{
+    typedef QTreeWidgetItem * qti_p;
+
+    /**
+      * if No files are selected, then selectall
+      */
+
+    if(recurse && selectedItems().isEmpty()){
+        m_selChangeLock = true;
+        selectAll();
+    }
+
+    const QList<qti_p> &selections(selectedItems());
+    Z88_Selection z88Selection;
+
+    QListIterator<qti_p> i(selections);
+
+    m_Selections.clear();
+
+    if(selections.count()){
+        while(i.hasNext()){
+            qti_p item(i.next());
+            getItemFspec(item, z88Selection);
+            m_Selections.append(z88Selection);
+            if(recurse){
+                /**
+                  * Recurse the Directory tree if any
+                  */
+                getItemChildren(item, m_Selections);
+            }
+        }
+    }
+    else{
+        /**
+          * Nothing selected, so get the Device name
+          */
+        getItemFspec(NULL, z88Selection);
+        m_Selections.append(z88Selection);
+    }
+
+    if(m_selChangeLock){
+        clearSelection();
+        m_selChangeLock = false;
+    }
+
+    return &m_Selections;
+}
+
---
+++ wsapps/eazylink2/z88_devview.h
@@ -0,0 +1,115 @@
+/*********************************************************************************************
+
+ EazyLink2 - Fast Client/Server Z88 File Management
+ (C) Gunther Strube (gstrube@gmail.com) 2011
+  & Oscar Ernohazy 2012
+
+ EazyLink2 is free software; you can redistribute it and/or modify it under the terms of the
+ GNU General Public License as published by the Free Software Foundation;
+ either version 2, or (at your option) any later version.
+ EazyLink2 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ See the GNU General Public License for more details.
+ You should have received a copy of the GNU General Public License along with EazyLink2;
+ see the file COPYING. If not, write to the
+ Free Software Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+**********************************************************************************************/
+#ifndef Z88_DEVVIEW_H
+#define Z88_DEVVIEW_H
+
+#include <QWidget>
+#include <QTreeWidget>
+#include <QBoxLayout>
+#include "commthread.h"
+
+class Z88_Selection;
+
+/**
+  * The Z88 Storage Device Tree View Class.
+  * Inherits from QTreeWidget Class.
+  */
+class Z88_DevView : public QTreeWidget
+{
+    Q_OBJECT
+public:
+    explicit Z88_DevView(const QString devname, CommThread &com_thread, QWidget *parent = 0);
+    
+    const QString &getDevname();
+
+    enum entryType{
+        type_Dir = QTreeWidgetItem::UserType + 1,
+        type_File
+    };
+
+    bool insertUniqueFspec(QStringList &fspec_list,
+                           entryType d_type,
+                           const QString &fsize,
+                           const QString &fcreate_date,
+                           const QString &fmod_date);
+
+    QList<Z88_Selection> *getSelection(bool recurse);
+
+    bool isSelChangeLocked(){return m_selChangeLock;}
+
+signals:
+    
+public slots:
+    
+protected:
+
+    bool insertFspecList(QTreeWidgetItem *parent,
+                         QStringList &fspec_list,
+                         entryType d_type,
+                         const QString &fsize,
+                         const QString &fcreate_date,
+                         const QString &fmod_date);
+
+    const Z88_Selection &getItemFspec(QTreeWidgetItem *item, Z88_Selection &fspec) const;
+
+    const QList<Z88_Selection> &getItemChildren(QTreeWidgetItem *item,
+                                                QList<Z88_Selection> &selections)const;
+
+    /**
+      * The Communications Thread.
+      */
+    CommThread &m_cthread;
+
+    QString    m_devname;
+
+    QList<Z88_Selection> m_Selections;
+
+    /**
+      * Flag to disable recursive events call-backs
+      * when auto-selection of copying entire Storage
+      * Device to the Desktop.
+      */
+    bool m_selChangeLock;
+};
+
+/**
+  * The Z88 Selected File Attribute Class.
+  * Container class to hold various attributes of a selected file on the Z88 View.
+  */
+class Z88_Selection{
+public:
+    Z88_Selection(QString fspec="", Z88_DevView::entryType type = Z88_DevView::type_Dir);
+
+    const QString &getFspec()const {return m_fspec;}
+    const Z88_DevView::entryType &getType() const{return m_type;}
+
+    friend class Z88_DevView;
+protected:
+    /**
+      * Selection file spec
+      */
+    QString m_fspec;
+
+    /**
+      * Selection type (dir or file)
+      */
+    Z88_DevView::entryType m_type;
+
+};
+
+#endif // Z88_DEVVIEW_H
