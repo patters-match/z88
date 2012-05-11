2b0229c3bf400a946d4ed7d9217c5e64e5560c80
Gunther Strube <gstrube@gmail.com>
2012-05-11 13:14:59 +0200

    /tools/ozvm, OZVM-8: Using Java Desktop to open external browser is a bad idea, since this API is not supported on all platforms. Use method as described here for Windows, Mac and Linux: http://www.mkyong.com/java/open-browser-in-java-windows-or-linux/

--- tools/ozvm/src/com/jira/cambridgez88/ozvm/Gui.java
+++ tools/ozvm/src/com/jira/cambridgez88/ozvm/Gui.java
@@ -205,19 +205,41 @@ public class Gui extends JFrame {
             userManualMenuItem.addActionListener(new ActionListener() {
 
                 public void actionPerformed(ActionEvent e) {
-                    java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+                    String os = System.getProperty("os.name").toLowerCase();
+                    Runtime rt = Runtime.getRuntime();
+                    String localHelpFilePath = OZvm.getInstance().getAppPath() + "help/index.html";
 
-                    if (!desktop.isSupported(java.awt.Desktop.Action.OPEN)) {
+                    try {
+                        if (os.indexOf("win") >= 0) {
+
+                            // this doesn't support showing urls in the form of "page.html#nameLink" 
+                            rt.exec("rundll32 url.dll,FileProtocolHandler " + localHelpFilePath);
+
+                        } else if (os.indexOf("mac") >= 0) {
+
+                            rt.exec("open " + localHelpFilePath);
+
+                        } else if (os.indexOf("nix") >= 0 || os.indexOf("nux") >= 0) {
+
+                            // Do a best guess on unix until we get a platform independent way
+                            // Build a list of browsers to try, in this order.
+                            String[] browsers = {"epiphany", "firefox", "mozilla", "konqueror",
+                                "netscape", "opera", "links", "lynx"};
+
+                            // Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
+                            StringBuffer cmd = new StringBuffer();
+                            for (int i = 0; i < browsers.length; i++) {
+                                cmd.append((i == 0 ? "" : " || ") + browsers[i] + " \"" + localHelpFilePath + "\" ");
+                            }
+
+                            rt.exec(new String[]{"sh", "-c", cmd.toString()});
+
+                        } 
+
+                    } catch (IOException ex) {
                         JOptionPane.showMessageDialog(Gui.this, errMsg, "OZvm Help", JOptionPane.ERROR_MESSAGE);
-                    } else {
-                        try {
-                            String localHelpFilePath = OZvm.getInstance().getAppPath() + "help/index.html";
-                            desktop.open(new File(localHelpFilePath));
-                        } catch (IOException ex) {
-                            JOptionPane.showMessageDialog(Gui.this, errMsg, "OZvm Help", JOptionPane.ERROR_MESSAGE);
-                        } catch (IllegalArgumentException ex2) {
-                            JOptionPane.showMessageDialog(Gui.this, errMsg, "OZvm Help", JOptionPane.ERROR_MESSAGE);
-                        }
+                    } catch (IllegalArgumentException ex2) {
+                        JOptionPane.showMessageDialog(Gui.this, errMsg, "OZvm Help", JOptionPane.ERROR_MESSAGE);
                     }
                 }
             });
