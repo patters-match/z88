115a495be3469eeb1f0ac8313fecdce5be3dc6e1
Gunther Strube <gstrube@gmail.com>
2014-11-20 18:31:24 +0100

    Updated AlarmSafe compile script to validate for Mpm V1.5 before compiling.

--- z88apps/alarmsafe/makeapp.bat
+++ z88apps/alarmsafe/makeapp.bat
@@ -16,8 +16,20 @@
 ::
 :: *************************************************************************************
 
+@echo off
+
 del *.obj *.sym *.bin *.map *.6? alarmsafe.epr
 
+:: return version of Mpm to command line environment.
+:: Only V1.5 or later of Mpm supports macros
+mpm -version 2>nul >nul
+if ERRORLEVEL 15 goto COMPILE_ALARMSAFE
+echo Mpm version is less than V1.5, AlarmSafe compilation aborted.
+echo Mpm displays the following:
+mpm
+goto END
+
+:COMPILE_ALARMSAFE
 :: Assemble the popdown and MTH
 mpm -b -I..\..\oz\def alarmsafe.asm
 
@@ -27,3 +39,4 @@ mpm -b -I..\..\oz\def romheader.asm
 :: Create a 16K Rom Card with AlarmSafe
 z88card -f alarmsafe.loadmap
 
+:END
